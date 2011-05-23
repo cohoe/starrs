828bcf6df4f635c54747ab16a8a2e67a60db43f3
Grant Cohoe <cohoe@csh.rit.edu>
2011-05-23 11:39:02 -0700

    removed api logging, altered ns_insert/update, moving date_modified to API

--- db/Triggers/triggers_dns.sql
+++ db/Triggers/triggers_dns.sql
@@ -3,7 +3,6 @@ CREATE OR REPLACE FUNCTION "dns"."a_insert"() RETURNS TRIGGER AS $$
 	DECLARE
 		RowCount	INTEGER;
 	BEGIN
-		SELECT api.create_log_entry('database','DEBUG','Begin dns.a_insert');
 		IF family(NEW."address")=4 THEN
 			NEW."type" := 'A';
 		ELSIF family(NEW."address")=6 THEN
@@ -16,8 +15,6 @@ CREATE OR REPLACE FUNCTION "dns"."a_insert"() RETURNS TRIGGER AS $$
 		AND NEW."address" << "ip"."subnets"."subnet";
 			
 		IF (RowCount < 1) THEN 
-			SELECT api.create_log_entry('database','ERROR','Address/Zone mismatch');
-			SELECT api.create_log_entry('database','ERROR',NEW);
 			RAISE EXCEPTION 'IP address and DNS Zone do not match (%, %)',NEW."address",NEW."zone";
 		END IF;
 		
@@ -31,7 +28,6 @@ CREATE OR REPLACE FUNCTION "dns"."a_update"() RETURNS TRIGGER AS $$
 DECLARE
 	RowCount	INTEGER;
 BEGIN
-	SELECT api.create_log_entry('database','DEBUG','Begin dns.a_update');
 	-- This function will auto fill in the DNS record type so you dont have to. It goes off of which IP family you are inserting.
 	IF NEW."address" != OLD."address" THEN
 		IF family(NEW."address")=4 THEN
@@ -46,8 +42,6 @@ BEGIN
 		AND NEW."address" << "ip"."subnets"."subnet";
 			
 		IF (RowCount < 1) THEN 
-			SELECT api.create_log_entry('database','ERROR','Address/Zone mismatch');
-			SELECT api.create_log_entry('database','ERROR',NEW);
 			RAISE EXCEPTION 'IP address and DNS Zone do not match (%, %)',NEW."address",NEW."zone";
 		END IF;
 	END IF;
@@ -59,13 +53,10 @@ BEGIN
 		AND NEW."address" << "ip"."subnets"."subnet";
 			
 		IF (RowCount < 1) THEN 
-			SELECT api.create_log_entry('database','ERROR','Address/Zone mismatch');
-			SELECT api.create_log_entry('database','ERROR',NEW);
 			RAISE EXCEPTION 'IP address and DNS Zone do not match (%, %)',NEW."address",NEW."zone";
 		END IF;
 	END IF;
 
-	NEW."date_modified" := current_timestamp;
 	RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
@@ -76,18 +67,14 @@ CREATE OR REPLACE FUNCTION "dns"."pointer_insert"() RETURNS TRIGGER AS $$
 	DECLARE
 		RowCount	INTEGER;
 	BEGIN
-		SELECT api.create_log_entry('database','DEBUG','Begin dns.pointer_insert');
 		-- Check if alias exists in A table
 		SELECT COUNT(*) INTO RowCount
 		FROM "dns"."a"
 		WHERE "dns"."a"."fqdn" = NEW."alias";
 		
 		IF (RowCount > 0) THEN
-			SELECT api.create_log_entry('database','ERROR','Alias name already exists');
-			SELECT api.create_log_entry('database','ERROR',NEW);
 			RAISE EXCEPTION 'Alias name (%) already exists',NEW."alias";
 		END IF;
-		SELECT api.create_log_entry('database','INFO','Creating new alias');
 		RETURN NEW;
 	END;
 $$ LANGUAGE 'plpgsql';
@@ -98,7 +85,6 @@ CREATE OR REPLACE FUNCTION "dns"."pointer_update"() RETURNS TRIGGER AS $$
 	DECLARE
 		RowCount	INTEGER;
 	BEGIN
-		SELECT api.create_log_entry('database','DEBUG','Begin dns.pointer_update');
 		IF NEW."alias" != OLD."alias" THEN
 			-- Check if alias exists in A table
 			SELECT COUNT(*) INTO RowCount
@@ -106,8 +92,6 @@ CREATE OR REPLACE FUNCTION "dns"."pointer_update"() RETURNS TRIGGER AS $$
 			WHERE "dns"."a"."fqdn" = NEW."alias";
 			
 			IF (RowCount > 0) THEN
-				SELECT api.create_log_entry('database','ERROR','Alias name already exists');
-				SELECT api.create_log_entry('database','ERROR',NEW);
 				RAISE EXCEPTION 'Alias name (%) already exists',NEW."alias";
 			END IF;
 		END IF;
@@ -119,57 +103,47 @@ CREATE OR REPLACE FUNCTION "dns"."pointer_update"() RETURNS TRIGGER AS $$
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "dns"."pointer_update"() IS 'Check if the new alias already exists as an address record';
 
-/* TRIGGER - ns_insert */
+/* Trigger - ns_insert 
+	1) Check for primary NS existance
+*/
 CREATE OR REPLACE FUNCTION "dns"."ns_insert" RETURNS TRIGGER AS $$
 	DECLARE
 		RowCount	INTEGER;
 	BEGIN
-		SELECT api.create_log_entry('database','INFO','Begin dns.ns_insert');
 		-- Check for existing primary NS for zone
 		SELECT COUNT(*) INTO RowCount
 		FROM "dns"."ns"
 		WHERE "dns"."ns"."zone" = NEW."zone" AND "dns"."ns"."isprimary" = TRUE;
-		
 		IF NEW."isprimary" = TRUE AND RowCount > 0 THEN
-			SELECT api.create_log_entry('database','ERROR','Primary NS for zone already exists');
-			SELECT api.create_log_entry('database','ERROR',NEW);
 			RAISE EXCEPTION 'Primary NS for zone already exists';
 		ELSIF NEW."isprimary" = FALSE AND RowCount = 0 THEN
-			SELECT api.create_log_entry('database','ERROR','No primary NS for zone already exists and this is not one');
-			SELECT api.create_log_entry('database','ERROR',NEW);
 			RAISE EXCEPTION 'No primary NS for zone exists, and this is not primary. You must specify a primary NS for a zone';
 		END IF;
 
-		SELECT api.create_log_entry('database','INFO','Creating new NS');
 		RETURN NEW;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "dns"."ns_insert"() IS 'Check that there is only one primary NS registered for a given zone';
 
-/* TRIGGER - ns_update */
+/* Trigger - ns_update 
+	1) Check for primary NS
+*/
 CREATE OR REPLACE FUNCTION "dns"."ns_update" RETURNS TRIGGER AS $$
 	DECLARE
 		RowCount	INTEGER;
 	BEGIN
 		-- Check for existing primary NS for zone
-		IF NEW."zone" != OLD."zone_id" THEN
+		IF NEW."zone" != OLD."zone" THEN
 			SELECT COUNT(*) INTO RowCount
 			FROM "dns"."ns"
-			WHERE "dns"."ns"."zone" = NEW."zone_id" AND "dns"."ns"."isprimary" = TRUE;
-			
+			WHERE "dns"."ns"."zone" = NEW."zone" AND "dns"."ns"."isprimary" = TRUE;
 			IF NEW."isprimary" = TRUE AND RowCount > 0 THEN
-				SELECT api.create_log_entry('database','ERROR','Primary NS for zone already exists');
-				SELECT api.create_log_entry('database','ERROR',NEW);
 				RAISE EXCEPTION 'Primary NS for zone already exists';
 			ELSIF NEW."isprimary" = FALSE AND RowCount = 0 THEN
-				SELECT api.create_log_entry('database','ERROR','No primary NS for zone already exists and this is not one');
-				SELECT api.create_log_entry('database','ERROR',NEW);
 				RAISE EXCEPTION 'No primary NS for zone exists, and this is not primary. You must specify a primary NS for a zone';
 			END IF;
 		END IF;
-
-		SELECT api.create_log_entry('database','INFO','Modifying NS');
-		NEW."date_modified" := current_timestamp;
+		
 		RETURN NEW;
 	END;
 $$ LANGUAGE 'plpgsql';
