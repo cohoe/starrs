3700d98ca665814a81254f22cf05b3579d3f28d7
Grant Cohoe <cohoe@csh.rit.edu>
2011-05-24 08:47:10 -0700

    added metahost functions

--- db/Triggers/triggers_firewall.sql
+++ db/Triggers/triggers_firewall.sql
@@ -35,3 +35,59 @@ CREATE OR REPLACE FUNCTION "firewall"."rules_update"() RETURNS TRIGGER AS $$
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "firewall"."rules_update"() IS 'Firewall rule verification';
+
+/* Trigger - metahost_members_insert
+	1) Remove old rules
+	2) Apply metahost rules
+*/
+CREATE OR REPLACE FUNCTION "firewall"."metahost_members_insert"() RETURNS TRIGGER AS $$
+	DECLARE
+		result record;
+	BEGIN
+		-- Remove old rules
+		DELETE FROM "firewall"."rules" WHERE "address" = NEW."address";
+		
+		-- Apply metahost rules
+		FOR result IN SELECT "port","transport","deny" FROM "firewall"."metahost_rules" WHERE "name" = NEW."name" LOOP
+			INSERT INTO "firewall"."rules" ("address","port","transport","deny","owner") VALUES 
+			(NEW."address",result.port,result.transport,result.deny,api.get_current_user());
+		END LOOP;
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "firewall"."metahost_members_insert"() IS 'Add an address to a firewall metahost';
+
+/* Trigger - metahost_members_delete
+	1) Remove old rules
+*/
+CREATE OR REPLACE FUNCTION "firewall"."metahost_members_delete"() RETURNS TRIGGER AS $$
+	DECLARE
+		result record;
+	BEGIN
+		-- Remove old rules
+		DELETE FROM "firewall"."rules" WHERE "address" = NEW."address";
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "firewall"."metahost_members_delete"() IS 'Delete an address from a firewall metahost';
+
+/* Trigger - metahost_members_update
+	1) Remove old address rules
+	2) Apply metahost rules
+*/
+CREATE OR REPLACE FUNCTION "firewall"."metahost_members_insert"() RETURNS TRIGGER AS $$
+	DECLARE
+		result record;
+	BEGIN
+		IF NEW."address" != OLD."address" OR NEW."name" != OLD."name" THEN
+			-- Remove old rules
+			DELETE FROM "firewall"."rules" WHERE "address" = OLD."address";
+			DELETE FROM "firewall"."rules" WHERE "address" = NEW."address";
+			
+			-- Apply metahost rules
+			FOR result IN SELECT "port","transport","deny" FROM "firewall"."metahost_rules" WHERE "name" = NEW."name" LOOP
+				INSERT INTO "firewall"."rules" ("address","port","transport","deny","owner") VALUES 
+				(NEW."address",result.port,result.transport,result.deny,api.get_current_user());
+			END LOOP;
+		END IF;
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "firewall"."metahost_members_insert"() IS 'Alter a metahost member';
