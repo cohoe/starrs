c45c2b481a33520d1458333552f93d6bcf83bc91
Grant Cohoe <cohoe@csh.rit.edu>
2011-05-25 09:08:14 -0700

    reordered routines of create_subnet

--- db/API/api_ip.sql
+++ db/API/api_ip.sql
@@ -14,15 +14,15 @@ CREATE OR REPLACE FUNCTION "api"."create_subnet"(input_subnet cidr, input_name t
 		input_name := api.sanitize_general(input_name);
 		input_comment := api.sanitize_general(input_comment);
 
-		-- Create RDNS zone
-		PERFORM api.create_log_entry('API','INFO','creating reverse zone for subnet');
-		PERFORM api.create_dns_zone(api.get_reverse_domain(input_subnet),NULL,FALSE,'Reverse zone for subnet '||text(input_subnet));
-
 		-- Create new subnet
 		PERFORM api.create_log_entry('API', 'INFO', 'creating new subnet');
 		INSERT INTO "ip"."subnets" 
 			("subnet","name","comment","autogen","last_modifier","owner") VALUES
 			(input_subnet,input_name,input_comment,input_autogen,api.get_current_user(),api.get_current_user());
+
+		-- Create RDNS zone
+		PERFORM api.create_log_entry('API','INFO','creating reverse zone for subnet');
+		PERFORM api.create_dns_zone(api.get_reverse_domain(input_subnet),NULL,FALSE,'Reverse zone for subnet '||text(input_subnet));
 
 		PERFORM api.create_log_entry('API', 'DEBUG', 'Finish api.create_subnet');
 	END;
