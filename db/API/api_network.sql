696b0004a75402c8e9f4f65c40f73b15e704b935
Grant Cohoe <cohoe@csh.rit.edu>
2011-05-25 05:55:39 -0700

    replace select with perform

--- db/API/api_network.sql
+++ db/API/api_network.sql
@@ -5,7 +5,7 @@
 */
 CREATE OR REPLACE FUNCTION "api"."create_switchport"(input_port_name text, input_system_name text, input_port_type text, input_description text) RETURNS VOID AS $$
 	BEGIN
-		SELECT api.create_log_entry('API','DEBUG','begin api.create_switchport');
+		PERFORM api.create_log_entry('API','DEBUG','begin api.create_switchport');
 		
 		-- Sanitize input
 		input_port_name := api.sanitize_general(input_port_name);
@@ -14,11 +14,11 @@ CREATE OR REPLACE FUNCTION "api"."create_switchport"(input_port_name text, input
 		input_description := api.sanitize_general(input_description);
 		
 		-- Create directive
-		SELECT api.create_log_entry('API','INFO','creating switchport');
+		PERFORM api.create_log_entry('API','INFO','creating switchport');
 		INSERT INTO "network"."switchports" ("port_name","system_name","type","description") VALUES 
 		(input_port_name, input_system_name, input_port_type, input_description);
 		
-		SELECT api.create_log_entry('API','DEBUG','finish api.create_switchport');
+		PERFORM api.create_log_entry('API','DEBUG','finish api.create_switchport');
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."create_switchport"(text, text, text, text) IS 'Create a new network switchport';
@@ -30,17 +30,17 @@ COMMENT ON FUNCTION "api"."create_switchport"(text, text, text, text) IS 'Create
 */
 CREATE OR REPLACE FUNCTION "api"."remove_switchport"(input_port_name text, input_system_name text) RETURNS VOID AS $$
 	BEGIN
-		SELECT api.create_log_entry('API','DEBUG','begin api.remove_switchport');
+		PERFORM api.create_log_entry('API','DEBUG','begin api.remove_switchport');
 		
 		-- Sanitize input
 		input_port_name := api.sanitize_general(input_port_name);
 		input_system_name := api.sanitize_general(input_system_name);
 		
 		-- Create directive
-		SELECT api.create_log_entry('API','INFO','removing switchport');
+		PERFORM api.create_log_entry('API','INFO','removing switchport');
 		DELETE FROM "network"."switchports" WHERE "port_name" = input_port_name AND "system_name" = input_system_name;
 		
-		SELECT api.create_log_entry('API','DEBUG','finish api.remove_switchport');
+		PERFORM api.create_log_entry('API','DEBUG','finish api.remove_switchport');
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."remove_switchport"(text, text) IS 'Remove a network switchport';
