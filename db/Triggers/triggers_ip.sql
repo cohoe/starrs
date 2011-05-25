355fbbdc1365ce6e6a3f9415eb855822a40841a2
Grant Cohoe <cohoe@csh.rit.edu>
2011-05-25 08:44:37 -0700

    commenting, formatting, various fixes

--- db/Triggers/triggers_ip.sql
+++ db/Triggers/triggers_ip.sql
@@ -1,47 +1,14 @@
-/* TRIGGER subnets_insert */
+/* Trigger subnets_insert 
+	1) Check for larger subnets
+	2) Check for smaller subnets
+	3) Check for existing addresses
+	4) Autogenerate addresses
+*/
 CREATE OR REPLACE FUNCTION "ip"."subnets_insert"() RETURNS TRIGGER AS $$
-DECLARE
-	RowCount INTEGER;	-- Placeholder
-BEGIN
-
-	-- Check for larger/smaller subnets and existing addresses
-	SELECT COUNT(*) INTO RowCount
-	FROM "ip"."subnets"
-	WHERE NEW."subnet" << "ip"."subnets"."subnet";
-	IF (RowCount > 0) THEN
-		RAISE EXCEPTION 'A larger existing subnet was detected. Nested subnets are not supported.';
-	END IF;
-
-	SELECT COUNT(*) INTO RowCount
-	FROM "ip"."subnets"
-	WHERE NEW."subnet" >> "ip"."subnets"."subnet";
-	IF (RowCount > 0) THEN
-		RAISE EXCEPTION 'A smaller existing subnet was detected. Nested subnets are not supported.';
-	END IF;
-	
-	SELECT COUNT(*) INTO RowCount
-	FROM "ip"."addresses"
-	WHERE "ip"."addresses"."address" << NEW."subnet";
-	IF RowCount >= 1 THEN
-		RAISE EXCEPTION 'Existing addresses detected for your subnet. Modify the existing subnet.';
-	END IF;
-
-	-- Autogenerate all IP addresses if told to
-	IF NEW."autogen" IS TRUE THEN
-		INSERT INTO "ip"."addresses" ("address","last_modifier") SELECT "get_subnet_addresses",api.get_current_user() FROM api.get_subnet_addresses(NEW."subnet");
-	END IF;
-RETURN NEW;
-END;
-$$ LANGUAGE 'plpgsql';
-
-/* TRIGGER - subnets_update */
-CREATE OR REPLACE FUNCTION "ip"."subnets_update"() RETURNS TRIGGER AS $$
-DECLARE
-	RowCount INTEGER;	-- Placeholder
-BEGIN
-
-	-- Check for larger/smaller subnets and existing addresses
-	IF NEW."subnet" != OLD."subnet" THEN
+	DECLARE
+		RowCount INTEGER;
+	BEGIN
+		-- Check for larger subnets
 		SELECT COUNT(*) INTO RowCount
 		FROM "ip"."subnets"
 		WHERE NEW."subnet" << "ip"."subnets"."subnet";
@@ -49,6 +16,7 @@ BEGIN
 			RAISE EXCEPTION 'A larger existing subnet was detected. Nested subnets are not supported.';
 		END IF;
 
+		-- Check for smaller subnets
 		SELECT COUNT(*) INTO RowCount
 		FROM "ip"."subnets"
 		WHERE NEW."subnet" >> "ip"."subnets"."subnet";
@@ -56,111 +24,168 @@ BEGIN
 			RAISE EXCEPTION 'A smaller existing subnet was detected. Nested subnets are not supported.';
 		END IF;
 		
+		-- Check for existing addresses
 		SELECT COUNT(*) INTO RowCount
 		FROM "ip"."addresses"
 		WHERE "ip"."addresses"."address" << NEW."subnet";
 		IF RowCount >= 1 THEN
 			RAISE EXCEPTION 'Existing addresses detected for your subnet. Modify the existing subnet.';
 		END IF;
-	END IF;
 
-	-- Autogenerate all IP addresses if told to
-	IF NEW."autogen" != OLD."autogen" THEN
+		-- Autogenerate addresses
 		IF NEW."autogen" IS TRUE THEN
-			DELETE FROM "ip"."addresses" WHERE "ip"."addresses"."address" << OLD."subnet";
-			INSERT INTO "ip"."addresses" ("address") SELECT * FROM ip_address_autopopulation(NEW."subnet");
+			INSERT INTO "ip"."addresses" ("address","last_modifier") SELECT "get_subnet_addresses",api.get_current_user() FROM api.get_subnet_addresses(NEW."subnet");
 		END IF;
-	END IF;
-	NEW."date_modified" := current_timestamp;
-RETURN NEW;
-END;
+		
+		-- Done
+		RETURN NEW;
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "ip"."subnets_insert"() IS 'Create a subnet';
+
+/* Trigger - subnets_update 
+	1) Check for larger subnets
+	2) Check for smaller subnets
+	3) Check for existing addresses
+	4) Autogenerate addresses
+*/
+CREATE OR REPLACE FUNCTION "ip"."subnets_update"() RETURNS TRIGGER AS $$
+	DECLARE
+		RowCount INTEGER;
+	BEGIN
+		IF NEW."subnet" != OLD."subnet" THEN
+			-- Check for larger subnets
+			SELECT COUNT(*) INTO RowCount
+			FROM "ip"."subnets"
+			WHERE NEW."subnet" << "ip"."subnets"."subnet";
+			IF (RowCount > 0) THEN
+				RAISE EXCEPTION 'A larger existing subnet was detected. Nested subnets are not supported.';
+			END IF;
+
+			-- Check for smaller subnets
+			SELECT COUNT(*) INTO RowCount
+			FROM "ip"."subnets"
+			WHERE NEW."subnet" >> "ip"."subnets"."subnet";
+			IF (RowCount > 0) THEN
+				RAISE EXCEPTION 'A smaller existing subnet was detected. Nested subnets are not supported.';
+			END IF;
+			
+			-- Check for existing addresses
+			SELECT COUNT(*) INTO RowCount
+			FROM "ip"."addresses"
+			WHERE "ip"."addresses"."address" << NEW."subnet";
+			IF RowCount >= 1 THEN
+				RAISE EXCEPTION 'Existing addresses detected for your subnet. Modify the existing subnet.';
+			END IF;
+		END IF;
+
+		-- Autogenerate addresses
+		IF NEW."autogen" != OLD."autogen" THEN
+			IF NEW."autogen" IS TRUE THEN
+				DELETE FROM "ip"."addresses" WHERE "ip"."addresses"."address" << OLD."subnet";
+				INSERT INTO "ip"."addresses" ("address") SELECT * FROM ip_address_autopopulation(NEW."subnet");
+			END IF;
+		END IF;
+		
+		-- Done
+		RETURN NEW;
+	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "ip"."subnets_update"() IS 'Modify an existing new subnet';
 
-/* TRIGGER - subnets_delete */
--- Delete an existing subnet, remove its addresses from the table. Only works if they arent in use.
--- BROKEN
+/* Trigger - subnets_delete 
+	1) Check for inuse addresses
+	2) Delete autogenerated addresses
+*/
 CREATE OR REPLACE FUNCTION "ip"."subnets_delete"() RETURNS TRIGGER AS $$
-DECLARE
-	RowCount INTEGER;	-- Placeholder
-BEGIN
-
-	IF OLD."autogen" = TRUE THEN
-		-- Check to see if there are inuse addresses from your old subnet. All addresses must be set not inuse before you can proceed.
+	DECLARE
+		RowCount INTEGER;
+	BEGIN
+		-- Check for inuse addresses
 		SELECT COUNT(*) INTO RowCount
 		FROM "ip"."addresses"
 		WHERE EXISTS (
 			SELECT "address" 
 			FROM "systems"."interface_addresses" 
-			WHERE "systems"."interface_addresses"."address" = "ip"."addresses"."address"
-		)
+			WHERE "systems"."interface_addresses"."address" = "ip"."addresses"."address" )
 		AND "ip"."addresses"."address" << OLD."subnet";
-		
 		IF (RowCount >= 1) THEN
 			RAISE EXCEPTION 'Inuse addresses found. Aborting delete.';
 		ELSE
-			-- Looks like you are good!
+
+		-- Delete autogenerated addresses
+		IF OLD."autogen" = TRUE THEN
 			DELETE FROM "ip"."addresses" WHERE "address" << OLD."subnet";
 		END IF;
-	END IF;
 
-	-- Delete RDNS zone for old subnet
-	DELETE FROM "dns"."zones" WHERE "dns"."zones"."zone" = api.get_reverse_domain(OLD."subnet");
-	RETURN OLD;
-END;
+		-- Done
+		RETURN OLD;
+	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "ip"."subnets_delete"() IS 'You can only delete a subnet if no addresses from it are inuse.';
 
-
-/* TRIGGER - addresses_insert */
+/* Trigger - addresses_insert 
+	1) Check for existing default action (should never happen)
+	2) Create action
+*/
 CREATE OR REPLACE FUNCTION "ip"."addresses_insert"() RETURNS TRIGGER AS $$
-DECLARE
-	RowCount INTEGER;
-BEGIN
-	-- Search the firewall defaults table for the address you are trying to add to make sure it doesnt already exist in there
-	SELECT COUNT(*) INTO RowCount
-	FROM "firewall"."defaults"
-	WHERE "firewall"."defaults"."address" = NEW."address";
-	
-	IF (RowCount >= 1) THEN
-		RAISE EXCEPTION 'Address % is already has a firewall default action?',NEW."address";
-	-- Looks like it does not. Insert it with the default value to DENY.
-	ELSIF (RowCount = 0) THEN
-		RETURN NEW;
-		INSERT INTO "firewall"."defaults" ("address", "deny", "last_modifier") VALUES (NEW."address", DEFAULT, NEW."last_modifier");
-	-- Not sure what is going on here. There's some funky crap going on.
-	ELSE
-		RAISE EXCEPTION 'Could not activate firewall address %',NEW."address";
-	END IF;
-END;
+	DECLARE
+		RowCount INTEGER;
+	BEGIN
+		-- Check for existing default action
+		SELECT COUNT(*) INTO RowCount
+		FROM "firewall"."defaults"
+		WHERE "firewall"."defaults"."address" = NEW."address";
+		
+		-- Create action
+		IF (RowCount >= 1) THEN
+			RAISE EXCEPTION 'Address % is already has a firewall default action?',NEW."address";
+		ELSIF (RowCount = 0) THEN
+			-- Insert the new address record, then the firewall. (Foreign keys)
+			RETURN NEW;
+			INSERT INTO "firewall"."defaults" ("address", "deny", "last_modifier") VALUES (NEW."address", DEFAULT, NEW."last_modifier");
+		ELSE
+			RAISE EXCEPTION 'Could not activate firewall address %',NEW."address";
+		END IF;
+	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "ip"."addresses_insert"() IS 'Activate a new IP address in the application';
 
-/* TRIGGER - ranges_insert */
+/* Trigger - ranges_insert 
+	1) Check for illegal addresses
+	2) Check address vs subnet
+	3) Check valid range
+	4) Check address existance
+	5) Define lower boundary for range
+	6) Define upper boundry for range
+	7) Check for range spanning
+*/
 CREATE OR REPLACE FUNCTION "ip"."ranges_insert"() RETURNS TRIGGER AS $$
 	DECLARE
-		LowerBound	INET;
-		UpperBound	INET;
-		result		RECORD;
-		RowCount	INTEGER;
+		LowerBound INET;
+		UpperBound INET;
+		result RECORD;
+		RowCount INTEGER;
 	BEGIN
-		-- Basic checks
+		-- Check for illegal addresses
 		IF host(input_subnet) = host(input_first_ip) THEN
 			RAISE EXCEPTION 'You cannot have a boundry that is the network identifier';
 		END IF;
 		
+		-- Check address vs subnet
 		IF NOT input_first_ip << input_subnet OR NOT input_last_ip << input_subnet THEN
 			RAISE EXCEPTION 'Range addresses must be inside the specified subnet';
 		END IF;
 
+		-- Check valid range
 		IF input_first_ip >= input_last_ip THEN
 			RAISE EXCEPTION 'First address is larger or equal to last address.';
 		END IF;
 		
+		-- Check address existance
 		SELECT COUNT(*) INTO RowCount
 		FROM "ip"."addresses"
 		WHERE "ip"."addresses"."address" = input_first_ip;
-		
 		IF (RowCount != 1) THEN
 			RAISE EXCEPTION 'First address (%) not found in address pool.',input_first_ip;
 		END IF;
@@ -168,11 +193,11 @@ CREATE OR REPLACE FUNCTION "ip"."ranges_insert"() RETURNS TRIGGER AS $$
 		SELECT COUNT(*) INTO RowCount
 		FROM "ip"."addresses"
 		WHERE "ip"."addresses"."address" = input_last_ip;
-		
 		IF (RowCount != 1) THEN
 			RAISE EXCEPTION 'Last address (%) not found in address pool.',input_last_ip;
 		END IF;
 
+		-- Define lower boundary for range
 		-- Loop through all ranges and find what is near the new range
 		FOR result IN SELECT "first_ip","last_ip" FROM "ip"."ranges" WHERE "subnet" = input_subnet LOOP
 			IF input_first_ip >= result.first_ip AND input_first_ip <= result.last_ip THEN
@@ -185,27 +210,32 @@ CREATE OR REPLACE FUNCTION "ip"."ranges_insert"() RETURNS TRIGGER AS $$
 			END IF;
 		END LOOP;
 
-		-- Define the upper bound of the range
+		-- Define upper boundry for range
 		SELECT "first_ip" INTO UpperBound
 		FROM "ip"."ranges"
 		WHERE "first_ip" >= LowerBound
 		ORDER BY "first_ip" LIMIT 1;
 
-		-- Check if we try to span multiple ranges
+		-- Check for range spanning
 		IF input_last_ip >= UpperBound THEN
 			RAISE EXCEPTION 'Last address is out of bounds';
 		END IF;
 
-		-- All is well. Insert range
+		-- Done
 		RETURN NEW;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "ip"."ranges_insert"() IS 'Insert a new range of addresses for use';
 
-
-
-
-/* TRIGGER - ranges_update */
+/* Trigger - ranges_update
+	1) Check for illegal addresses
+	2) Check address vs subnet
+	3) Check valid range
+	4) Check address existance
+	5) Define lower boundary for range
+	6) Define upper boundry for range
+	7) Check for range spanning
+*/
 CREATE OR REPLACE FUNCTION "ip"."ranges_update"() RETURNS TRIGGER AS $$
 	DECLARE
 		LowerBound	INET;
@@ -214,23 +244,25 @@ CREATE OR REPLACE FUNCTION "ip"."ranges_update"() RETURNS TRIGGER AS $$
 		RowCount	INTEGER;
 	BEGIN
 		IF NEW."first_ip" != OLD."first_ip" OR NEW."last_ip" != OLD."last_ip" THEN
-			-- Basic checks
+			-- Check for illegal addresses
 			IF host(input_subnet) = host(input_first_ip) THEN
 				RAISE EXCEPTION 'You cannot have a boundry that is the network identifier';
 			END IF;
 			
+			-- Check address vs subnet
 			IF NOT input_first_ip << input_subnet OR NOT input_last_ip << input_subnet THEN
 				RAISE EXCEPTION 'Range addresses must be inside the specified subnet';
 			END IF;
 
+			-- Check valid range
 			IF input_first_ip >= input_last_ip THEN
 				RAISE EXCEPTION 'First address is larger or equal to last address.';
 			END IF;
 			
+			-- Check address existance
 			SELECT COUNT(*) INTO RowCount
 			FROM "ip"."addresses"
 			WHERE "ip"."addresses"."address" = input_first_ip;
-			
 			IF (RowCount != 1) THEN
 				RAISE EXCEPTION 'First address (%) not found in address pool.',input_first_ip;
 			END IF;
@@ -238,11 +270,11 @@ CREATE OR REPLACE FUNCTION "ip"."ranges_update"() RETURNS TRIGGER AS $$
 			SELECT COUNT(*) INTO RowCount
 			FROM "ip"."addresses"
 			WHERE "ip"."addresses"."address" = input_last_ip;
-			
 			IF (RowCount != 1) THEN
 				RAISE EXCEPTION 'Last address (%) not found in address pool.',input_last_ip;
 			END IF;
 
+			-- Define lower boundary for range
 			-- Loop through all ranges and find what is near the new range
 			FOR result IN SELECT "first_ip","last_ip" FROM "ip"."ranges" WHERE "subnet" = input_subnet LOOP
 				IF input_first_ip >= result.first_ip AND input_first_ip <= result.last_ip THEN
@@ -255,19 +287,18 @@ CREATE OR REPLACE FUNCTION "ip"."ranges_update"() RETURNS TRIGGER AS $$
 				END IF;
 			END LOOP;
 
-			-- Define the upper bound of the range
+			-- Define upper boundry for range
 			SELECT "first_ip" INTO UpperBound
 			FROM "ip"."ranges"
 			WHERE "first_ip" >= LowerBound
 			ORDER BY "first_ip" LIMIT 1;
 
-			-- Check if we try to span multiple ranges
+			-- Check for range spanning
 			IF input_last_ip >= UpperBound THEN
 				RAISE EXCEPTION 'Last address is out of bounds';
 			END IF;
-
-			-- All is well. Insert range
 		END IF;
+		-- Done
 		RETURN NEW;
 	END;
 $$ LANGUAGE 'plpgsql';
