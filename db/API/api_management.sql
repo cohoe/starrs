54b906c9239749f5ef1d107f1378a86a8b027533
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-01 09:24:07 -0700

    added , to general

--- db/API/api_management.sql
+++ db/API/api_management.sql
@@ -22,7 +22,7 @@ CREATE OR REPLACE FUNCTION "api"."sanitize_general"(input text) RETURNS TEXT AS 
 	DECLARE
 		BadCrap TEXT;
 	BEGIN
-		BadCrap = regexp_replace(input, E'[a-z0-9\_\.\:\/ \(\)]*\-*', '', 'gi');
+		BadCrap = regexp_replace(input, E'[a-z0-9\_\,\.\:\/ \(\)]*\-*', '', 'gi');
 		IF BadCrap != '' THEN
 			RAISE EXCEPTION 'Invalid characters detected in string "%"',input;
 		END IF;
