7796b073afaf4f8a55a442a7b15af66702de5b7a
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-01 08:14:11 -0700

    Added comments, removed views (need to be in separate file)

--- DB/Schema/Setup.sql
+++ DB/Schema/Setup.sql
@@ -1,31 +1,39 @@
-set search_path TO "public";
 
 /*Schema firewall*/
 CREATE SCHEMA "firewall";
+COMMENT ON SCHEMA "firewall" IS 'All firewall data controlling access from the outside world';
 
 /*Schema systems*/
 CREATE SCHEMA "systems";
+COMMENT ON SCHEMA "systems" IS 'User machine data for devices on the network';
 
 /*Schema dhcp*/
 CREATE SCHEMA "dhcp";
+COMMENT ON SCHEMA "dhcp" IS 'Configuration for stateful addressing';
 
 /*Schema ip*/
 CREATE SCHEMA "ip";
+COMMENT ON SCHEMA "ip" IS 'Network resources available for devices';
 
 /*Schema dns*/
 CREATE SCHEMA "dns";
+COMMENT ON SCHEMA "dns" IS 'All DNS records for the controlled zones/domains';
 
 /*Schema management*/
 CREATE SCHEMA "management";
+COMMENT ON SCHEMA "management" IS 'Application configuration and data';
 
 /*Schema network*/
 CREATE SCHEMA "network";
+COMMENT ON SCHEMA "network" IS 'Data on special network devices';
 
 /*Schema api*/
 CREATE SCHEMA "api";
+COMMENT ON SCHEMA "api" IS 'Interaction with clients';
 
 /*Sequence Output ID*/
 CREATE SEQUENCE "management"."output_id_seq";
+COMMENT ON SEQUENCE "management"."output_id_seq" IS 'Identifier for all output results';
 
 /*Language plperl*/
 CREATE LANGUAGE "plperl";
@@ -33,17 +41,4 @@ CREATE LANGUAGE "plperl";
 /*Language plperlu*/
 CREATE LANGUAGE "plperlu";
 
-/*View Log - Master Debug*/
-CREATE OR REPLACE VIEW "management"."log_master_debug" AS SELECT * FROM "management"."log_master" WHERE "severity" LIKE 'DEBUG';
-
-COMMENT ON VIEW "management"."log_master_debug" IS 'View all DEBUG level data (all function calls and data)';
-
-/*View Log - Master Info*/
-CREATE OR REPLACE VIEW "management"."log_master_info" AS SELECT * FROM "management"."log_master" WHERE "severity" LIKE 'INFO';
-
-COMMENT ON VIEW "management"."log_master_info" IS 'View all INFO level data (when something happens)';
-
-/*View Log - Master Error*/
-CREATE OR REPLACE VIEW "management"."log_master_error" AS SELECT * FROM "management"."log_master" WHERE "severity" LIKE 'ERROR';
-
-COMMENT ON VIEW "management"."log_master_error" IS 'View all ERROR level data (there was an exception in some function)';
+set search_path TO "api";
