2d4c57a3f1b90d29000942bb68a9e6e4e525e77d
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-01 06:42:09 -0700

    added create_switchport_range

--- db/API/api_network.sql
+++ db/API/api_network.sql
@@ -44,3 +44,32 @@ CREATE OR REPLACE FUNCTION "api"."remove_switchport"(input_port_name text, input
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."remove_switchport"(text, text) IS 'Remove a network switchport';
+
+/* API - create_switchport_range
+	1) Sanitize input
+	2) Check privileges
+	3) Create ports
+*/
+CREATE OR REPLACE FUNCTION "api"."create_switchport_range"(input_prefix text, first_port integer, last_port integer, input_system_name text, input_port_type text, input_description text) RETURNS VOID AS $$
+	DECLARE
+		Counter INTEGER;
+	BEGIN
+		PERFORM api.create_log_entry('API','DEBUG','begin api.create_switchport_range');
+
+		-- Sanitize input
+		input_prefix := api.sanitize_general(input_prefix);
+		input_system_name := api.sanitize_general(input_system_name);
+		input_port_type := api.sanitize_general(input_port_type);
+		input_description := api.sanitize_general(input_description);
+		Counter := first_port;
+
+		-- Create ports
+		PERFORM api.create_log_entry('API','INFO','creating lots of switchports');
+		WHILE Counter != last_port + 1 LOOP
+			PERFORM api.create_switchport(input_prefix||Counter, input_system_name, input_port_type, input_description);
+			Counter := Counter + 1;
+		END LOOP;
+		
+		PERFORM api.create_log_entry('API','DEBUG','finish api.create_switchport_range');
+	END;
+$$ LANGUAGE 'plpgsql';
