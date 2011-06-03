9289a00ca09a8239c6a8995ea92b8904de4f8381
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-03 05:41:54 -0700

    added owner input to functions

--- DB/API/api_firewall.sql
+++ DB/API/api_firewall.sql
@@ -58,23 +58,28 @@ COMMENT ON FUNCTION "api"."modify_firewall_default"(inet, boolean) IS 'modify an
 	2) Sanitize input
 	3) Create metahost
 */
-CREATE OR REPLACE FUNCTION "api"."create_firewall_metahost"(input_name text, input_comment text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "api"."create_firewall_metahost"(input_name text, input_owner text, input_comment text) RETURNS VOID AS $$
 	BEGIN
 		PERFORM api.create_log_entry('API','DEBUG','begin api.create_firewall_metahost');
 
 		-- Sanitize input
 		input_name := api.sanitize_general(input_name);
 		input_comment := api.sanitize_general(input_comment);
+		input_owner := api.sanitize_general(input_owner);
+		
+		IF input_owner IS NULL THEN
+			input_owner := api.get_current_user();
+		END IF;
 		
 		-- Create metahost
 		PERFORM api.create_log_entry('API','INFO','creating new metahost');
 		INSERT INTO "firewall"."metahosts" ("name","comment","owner") VALUES 
-		(input_name, input_comment, api.get_current_user());
+		(input_name, input_comment, input_owner);
 		
 		PERFORM api.create_log_entry('API','DEBUG','finish api.create_firewall_metahost');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."create_firewall_metahost"(text, text) IS 'create a firewall metahost';
+COMMENT ON FUNCTION "api"."create_firewall_metahost"(text, text, text) IS 'create a firewall metahost';
 
 /* API - remove_firewall_metahost
 	1) Check privileges
@@ -101,7 +106,7 @@ COMMENT ON FUNCTION "api"."remove_firewall_metahost"(text) IS 'remove a firewall
 	2) Sanitize input
 	3) Create rule
 */
-CREATE OR REPLACE FUNCTION "api"."create_firewall_metahost_rule"(input_name text, input_port integer, input_transport text, input_deny boolean, input_comment text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "api"."create_firewall_metahost_rule"(input_name text, input_port integer, input_transport varchar(4), input_deny boolean, input_owner text, input_comment text) RETURNS VOID AS $$
 	BEGIN
 		PERFORM api.create_log_entry('API','DEBUG','begin create_firewall_metahost_rule');
 		
@@ -109,16 +114,21 @@ CREATE OR REPLACE FUNCTION "api"."create_firewall_metahost_rule"(input_name text
 		input_name := api.sanitize_general(input_name);
 		input_transport := api.sanitize_general(input_transport);
 		input_comment := api.sanitize_general(input_comment);
+		input_owner := api.sanitize_general(input_owner);
+		
+		IF input_owner IS NULL THEN
+			input_owner := api.get_current_user();
+		END IF;
 		
 		-- Create rule
 		PERFORM api.create_log_entry('API','INFO','creating new rule');
-		INSERT INTO "firewall"."metahost_rules" ("name","port","transport","deny","comment")
-		VALUES (input_name, input_port, input_transport, input_deny, input_comment);
+		INSERT INTO "firewall"."metahost_rules" ("name","port","transport","deny","comment","owner")
+		VALUES (input_name, input_port, input_transport, input_deny, input_comment, input_owner);
 		
 		PERFORM api.create_log_entry('API','DEBUG','finish create_firewall_metahost_rule');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."create_firewall_metahost_rule"(text, integer, text, boolean, text) IS 'Create a firewall metahost rule';
+COMMENT ON FUNCTION "api"."create_firewall_metahost_rule"(text, integer, varchar(4), boolean, text, text) IS 'Create a firewall metahost rule';
 
 /* API - remove_firewall_metahost_rule
 	1) Check privileges
@@ -190,23 +200,24 @@ COMMENT ON FUNCTION "api"."remove_firewall_system"(text) IS 'Remove a firewall s
 	2) Sanitize input
 	3) Create rule
 */
-CREATE OR REPLACE FUNCTION "api"."create_firewall_rule"(input_address inet, input_port integer, input_transport text, input_deny boolean, input_comment text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "api"."create_firewall_rule"(input_address inet, input_port integer, input_transport varchar(4), input_deny boolean, input_owner text, input_comment text) RETURNS VOID AS $$
 	BEGIN
 		PERFORM api.create_log_entry('API','DEBUG','begin create_firewall_rule');
 		
 		-- Sanitize input
 		input_transport := api.sanitize_general(input_transport);
 		input_comment := api.sanitize_general(input_comment);
+		input_owner := api.sanitize_general(input_owner);
 		
 		-- Create rule
 		PERFORM api.create_log_entry('API','INFO','creating firewall rule');
 		INSERT INTO "firewall"."rules" ("address","port","transport","deny","comment","owner")
-		VALUES (input_address, input_port, input_transport, input_deny, input_comment, api.get_current_user());
+		VALUES (input_address, input_port, input_transport, input_deny, input_comment, input_owner);
 		
 		PERFORM api.create_log_entry('API','DEBUG','finish create_firewall_rule');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."create_firewall_rule"(inet, integer, text, boolean, text) IS 'Create a standalone firewall rule';
+COMMENT ON FUNCTION "api"."create_firewall_rule"(inet, integer, varchar(4), boolean, text, text) IS 'Create a standalone firewall rule';
 
 /* API - remove_firewall_rule
 	1) Check privileges
@@ -253,7 +264,7 @@ COMMENT ON FUNCTION "api"."get_firewall_site_default"() IS 'Return the value of 
 	3) Get program information
 	4) Create rule
 */
-CREATE OR REPLACE FUNCTION "api"."create_firewall_rule_program"(input_address inet, input_program text, input_deny boolean) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "api"."create_firewall_rule_program"(input_address inet, input_program text, input_deny boolean, input_owner text) RETURNS VOID AS $$
 	DECLARE
 		Port INTEGER;
 		Transport VARCHAR(4);
@@ -262,6 +273,7 @@ CREATE OR REPLACE FUNCTION "api"."create_firewall_rule_program"(input_address in
 
 		-- Sanitize input
 		input_program := api.sanitize_general(input_program);
+		input_owner := api.sanitize_general(input_owner);
 
 		-- Get program information
 		SELECT "firewall"."programs"."port","firewall"."programs"."transport" INTO Port,Transport
@@ -271,14 +283,14 @@ CREATE OR REPLACE FUNCTION "api"."create_firewall_rule_program"(input_address in
 		-- Create rule
 		PERFORM api.create_log_entry('API','INFO','creating new rule from program');
 		INSERT INTO "firewall"."rules"
-		("address","port","transport","deny","owner") VALUES
-		(input_address,Port,Transport,input_deny,api.get_current_user());
+		("address","port","transport","deny","owner","comment") VALUES
+		(input_address,Port,Transport,input_deny,input_owner,'Program on port '||Port||' '||Transport);
 		
 		-- Done
 		PERFORM api.create_log_entry('API','DEBUG','finish create_firewall_rule_program');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."create_firewall_rule_program"(inet, text, boolean) IS 'Create a firewall rule based on a common program.';
+COMMENT ON FUNCTION "api"."create_firewall_rule_program"(inet, text, boolean, text) IS 'Create a firewall rule based on a common program.';
 
 /* API - remove_firewall_rule_program
 	1) Sanitize input
