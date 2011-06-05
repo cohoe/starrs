d28dc5cf10aebce337595d3d2b84b95f9f9f142a
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-05 00:26:20 -0400

    added get_user_level details, modified sanitize and initialize, i really like Tron Legacy

--- DB/API/api_management.sql
+++ DB/API/api_management.sql
@@ -22,14 +22,14 @@ CREATE OR REPLACE FUNCTION "api"."sanitize_general"(input text) RETURNS TEXT AS 
 	DECLARE
 		BadCrap TEXT;
 	BEGIN
-		BadCrap = regexp_replace(input, E'[a-z0-9\_\,\.\:\/ \(\)]*\-*', '', 'gi');
+		BadCrap = regexp_replace(input, E'[a-z0-9\_\,\.\:\/ \(\)=\*]*\-*', '', 'gi');
 		IF BadCrap != '' THEN
 			RAISE EXCEPTION 'Invalid characters detected in string "%"',input;
 		END IF;
 		RETURN input;
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."sanitize_general"(text) IS 'Allow only certain characters for most common objects';
+COMMENT ON FUNCTION "api"."sanitize_general"(text) IS 'Allow only certain characters for most common objects'
 
 /* API - sanitize_dhcp*/
 CREATE OR REPLACE FUNCTION "api"."sanitize_dhcp"(input text) RETURNS TEXT AS $$
@@ -207,7 +207,9 @@ COMMENT ON FUNCTION "api"."unlock_process"(text) IS 'Unlock a process for a job'
 	2) Create privilege table
 	3) Populate privileges
 */
-CREATE OR REPLACE FUNCTION "api"."initialize"(input_username text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "api"."initialize"(input_username text) RETURNS TEXT AS $$
+	DECLARE
+		Level TEXT;
 	BEGIN
 		-- Sanitize input
 		input_username := api.sanitize_general(input_username);
@@ -217,8 +219,17 @@ CREATE OR REPLACE FUNCTION "api"."initialize"(input_username text) RETURNS VOID 
 		(username text NOT NULL,privilege text NOT NULL,
 		allow boolean NOT NULL DEFAULT false);
 
+		-- Get level
+		SELECT api.get_user_level(input_username) INTO Level;
+		
 		-- Populate privileges
 		INSERT INTO user_privileges VALUES (input_username,'USERNAME',TRUE);
+
+		-- Done
+		IF Level='NONE' THEN
+			RAISE EXCEPTION 'Could not identify "%".',input_username;
+		END IF;
+		RETURN 'Greetings '||lower(Level)||'!';
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."initialize"(text) IS 'Setup user access to the database';
@@ -227,6 +238,7 @@ COMMENT ON FUNCTION "api"."initialize"(text) IS 'Setup user access to the databa
 	1) Load configuration
 	2) Bind to the LDAP server
 	3) Figure out permission level
+	4) Unbind from LDAP server
 */
 CREATE OR REPLACE FUNCTION "api"."get_user_level"(TEXT) RETURNS TEXT AS $$
 	use strict;
@@ -238,32 +250,68 @@ CREATE OR REPLACE FUNCTION "api"."get_user_level"(TEXT) RETURNS TEXT AS $$
 	my $host = spi_exec_query("SELECT api.get_site_configuration('LDAP_HOST')")->{rows}[0]->{"get_site_configuration"};
 	my $binddn = spi_exec_query("SELECT api.get_site_configuration('LDAP_BINDDN')")->{rows}[0]->{"get_site_configuration"};
 	my $password = spi_exec_query("SELECT api.get_site_configuration('LDAP_PASSWORD')")->{rows}[0]->{"get_site_configuration"};
-	my $group = spi_exec_query("SELECT api.get_site_configuration('LDAP_GROUP')")->{rows}[0]->{"get_site_configuration"};
-	my $basedn = spi_exec_query("SELECT api.get_site_configuration('LDAP_BASEDN')")->{rows}[0]->{"get_site_configuration"};
+	my $admin_filter = spi_exec_query("SELECT api.get_site_configuration('LDAP_ADMIN_FILTER')")->{rows}[0]->{"get_site_configuration"};
+	my $admin_basedn = spi_exec_query("SELECT api.get_site_configuration('LDAP_ADMIN_BASEDN')")->{rows}[0]->{"get_site_configuration"};
+	my $program_filter = spi_exec_query("SELECT api.get_site_configuration('LDAP_PROGRAM_FILTER')")->{rows}[0]->{"get_site_configuration"};
+	my $program_basedn = spi_exec_query("SELECT api.get_site_configuration('LDAP_PROGRAM_BASEDN')")->{rows}[0]->{"get_site_configuration"};
+	my $user_filter = spi_exec_query("SELECT api.get_site_configuration('LDAP_USER_FILTER')")->{rows}[0]->{"get_site_configuration"};
+	my $user_basedn = spi_exec_query("SELECT api.get_site_configuration('LDAP_USER_BASEDN')")->{rows}[0]->{"get_site_configuration"};
 
 	# The lowest status. Build from here.
 	my $status = "NONE";
 
-	# Bind to the LDAP server and search for the list of admins
+	# Bind to the LDAP server
 	my $srv = Net::LDAP->new ($host) or die "Could not connect to LDAP server ($host)\n";
 	my $mesg = $srv->bind($binddn,password=>$password) or die "Could not bind to LDAP server at $host\n";
-	$mesg = $srv->search(filter=>"($group)",base=>$basedn,attrs=>['member']);
-
-	# Go through the list and see if this user is an admin
+	
+	# Go through the directory and see if this user is a user account
+	$mesg = $srv->search(filter=>"($user_filter=$username)",base=>$user_basedn,attrs=>[$user_filter]);
 	foreach my $entry ($mesg->entries)
 	{
-		my @users = $entry->get_value("member");
+		my @users = $entry->get_value($user_filter);
 		foreach my $user (@users)
 		{
 			$user =~ s/^uid=(.*?)\,(.*?)$/$1/;
 			if ($user eq $username)
+			{
+				$status = "USER";
+			}
+		}
+	}
+
+	# Go through the directory and see if this user is a program account
+	$mesg = $srv->search(filter=>"($program_filter=$username)",base=>$program_basedn,attrs=>[$program_filter]);
+	foreach my $entry ($mesg->entries)
+	{
+		my @programs = $entry->get_value($program_filter);
+		foreach my $program (@programs)
+		{
+			if ($program eq $username)
+			{
+				$status = "PROGRAM";
+			}
+		}
+	}
+	
+	# Go through the directory and see if this user is an admin
+	# Fancy hacks to allow for less hardcoding of attributes
+	my $admin_filter_atr = $admin_filter;
+	$admin_filter_atr =~ s/^(.*?)[^a-zA-Z0-9]+$/$1/;
+	$mesg = $srv->search(filter=>"($admin_filter)",base=>$admin_basedn,attrs=>[$admin_filter_atr]);
+	foreach my $entry ($mesg->entries)
+	{
+		my @admins = $entry->get_value($admin_filter_atr);
+		foreach my $admin (@admins)
+		{
+			$admin =~ s/^uid=(.*?)\,(.*?)$/$1/;
+			if ($admin eq $username)
 			{
 				$status = "ADMIN";
 			}
 		}
 	}
 
-	# Disconnect from the LDAP server
+	# Unbind from the LDAP server
 	$srv->unbind;
 
 	# Done
