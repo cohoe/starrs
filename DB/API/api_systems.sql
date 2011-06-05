95a14d4f230ba38dea812564a2a3c99d89acb647
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-05 14:57:04 -0400

    added get_operating_systems and get_system_types

--- DB/API/api_systems.sql
+++ DB/API/api_systems.sql
@@ -141,3 +141,24 @@ CREATE OR REPLACE FUNCTION "api"."remove_interface_address"(input_address inet) 
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."remove_interface_address"(inet) IS 'delete an interface address';
+
+/* API - get_system_types
+	1) Return all available system types
+*/
+CREATE OR REPLACE FUNCTION "api"."get_system_types"() RETURNS SETOF TEXT AS $$
+	BEGIN
+		RETURN QUERY (SELECT "type" FROM "systems"."device_types");
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "api"."get_system_types"() IS 'Get a list of all available system types';
+
+/* API - get_operating_systems
+	1) Return all available operating systems
+*/
+CREATE OR REPLACE FUNCTION "api"."get_operating_systems"() RETURNS SETOF TEXT AS $$
+	BEGIN
+		RETURN QUERY (SELECT "name" FROM "systems"."os");
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "api"."get_operating_systems"() IS 'Get a list of all available system types';
+
