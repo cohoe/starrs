2070e5fd897289eb2b936828777fd9af2639e871
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-08 05:39:52 -0700

    fixed broken things

--- DB/API/Systems/api_systems_remove.sql
+++ DB/API/Systems/api_systems_remove.sql
@@ -11,18 +11,18 @@
 CREATE OR REPLACE FUNCTION "api"."remove_system"(input_system_name text) RETURNS VOID AS $$
 	BEGIN
 		PERFORM api.create_log_entry('API', 'DEBUG', 'Begin api.remove_system');
-		
+
 		-- Check privileges
 		IF api.get_current_user_level() ~* 'USER|PROGRAM' THEN
 			IF (SELECT "owner" FROM "systems"."systems" WHERE "system_name" = input_system_name) != api.get_current_user() THEN
 				RAISE EXCEPTION 'Permission denied on system %. You are not owner.',input_system_name;
 			END IF;
 		END IF;
-		
+
 		-- Remove system
 		PERFORM api.create_log_entry('API', 'INFO', 'Deleting system');
 		DELETE FROM "systems"."systems" WHERE "system_name" = input_system_name;
-		
+
 		-- Done
 		PERFORM api.create_log_entry('API', 'DEBUG', 'finish api.remove_system');
 	END;
@@ -36,20 +36,20 @@ COMMENT ON FUNCTION "api"."remove_system"(text) IS 'Delete an existing system';
 CREATE OR REPLACE FUNCTION "api"."remove_interface"(input_mac macaddr) RETURNS VOID AS $$
 	BEGIN
 		PERFORM api.create_log_entry('API','DEBUG','begin api.remove_interface');
-		
+
 		-- Check privileges
 		IF api.get_current_user_level() ~* 'USER|PROGRAM' THEN
 			IF (SELECT "owner" FROM "systems"."interfaces" 
 			JOIN "systems"."systems" ON "systems"."systems"."system_name" = "systems"."interfaces"."system_name"
-			WHERE "system_name" = input_mac) != api.get_current_user() THEN
+			WHERE "systems"."interfaces"."mac" = input_mac) != api.get_current_user() THEN
 				RAISE EXCEPTION 'Permission denied on interface %. You are not owner.',input_mac;
 			END IF;
 		END IF;
-		
+
 		-- Remove interface
 		PERFORM api.create_log_entry('API','INFO','deleting interface');
 		DELETE FROM "systems"."interfaces" WHERE "mac" = input_mac;
-		
+
 		-- Done
 		PERFORM api.create_log_entry('API','DEBUG','finish api.remove_interface');
 	END;
@@ -63,20 +63,21 @@ COMMENT ON FUNCTION "api"."remove_interface"(macaddr) IS 'delete an interface ba
 CREATE OR REPLACE FUNCTION "api"."remove_interface_address"(input_address inet) RETURNS VOID AS $$
 	BEGIN
 		PERFORM api.create_log_entry('API','DEBUG','begin api.remove_interface_address');
-		
+
 		-- Check privileges
 		IF api.get_current_user_level() ~* 'USER|PROGRAM' THEN
-			IF (SELECT "owner" FROM "systems"."interfaces" 
+			IF (SELECT "owner" FROM "systems"."interface_addresses" 
+			JOIN "systems"."interfaces" ON "systems"."interfaces"."mac" = "systems"."interface_addresses"."mac"
 			JOIN "systems"."systems" ON "systems"."systems"."system_name" = "systems"."interfaces"."system_name"
-			WHERE "system_name" = input_mac) != api.get_current_user() THEN
+			WHERE "systems"."interface_addresses"."address" = input_address) != api.get_current_user() THEN
 				RAISE EXCEPTION 'Permission denied on interface address %. You are not owner.',input_address;
 			END IF;
 		END IF;
-		
+
 		-- Remove address
 		PERFORM api.create_log_entry('API','INFO','deleting interface address');
 		DELETE FROM "systems"."interface_addresses" WHERE "address" = input_address;
-		
+
 		-- Done
 		PERFORM api.create_log_entry('API','DEBUG','begin api.remove_interface_address');
 	END;
