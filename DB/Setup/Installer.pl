6ae9b311f68dcc795a68964451f20c0f2b302d3e
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-07 21:35:12 -0400

    no idea

---
+++ DB/Setup/Installer.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Cwd;
+
+my $dbname = "impulse";
+
+my @files = ('create','remove','get','modify','utility');
+my @schemas = ('dhcp','dns','firewall','ip','management','network','systems');
+
+my $dir = getcwd();
+
+print "Jumpstarting...\n";
+system("psql","-f","\"$dir/Setup/Jumpstart.sql\"");
+print "Setup...\n";
+system("psql","-f","\"$dir/Setup/Setup.sql\"",$dbname);
+
+foreach my $schema (@schemas)
+{
+	foreach my $file (@files)
+	{
+		print "API $schema $file\n";
+		system("psql","-f","\"$dir/API/$schema/api\_$schema\_$file.sql\"",$dbname);
+	}
+}
+
+print "Tables...\n";
+system("psql","-f","\"$dir/Setup/Tables.sql\"",$dbname);
+print "Constraints...\n";
+system("psql","-f","\"$dir/Setup/Constraints.sql\"",$dbname);
+
+foreach my $schema (@schemas)
+{
+	print "Triggers for $schema\n";
+	system("psql","-f","\"$dir/Triggers/triggers\_$schema.sql\"",$dbname);
+}
+
+print "Triggers...\n";
+system("psql","-f","\"$dir/Triggers/Triggers.sql\"",$dbname);
+
+print "Views...\n";
+system("psql","-f","\"$dir/Setup/Views.sql\"",$dbname);
+print "Base...\n";
+system("psql","-f","\"$dir/Setup/Base.sql\"",$dbname);
+print "Privileges...\n";
+system("psql","-f","\"$dir/Setup/Privileges.sql\"",$dbname);
