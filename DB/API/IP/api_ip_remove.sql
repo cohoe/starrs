21c3eb81eb1419026989cce4af00bbf2c5362631
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-09 10:21:42 -0700

    added permissions (sorta forgot this file)

--- DB/API/IP/api_ip_remove.sql
+++ DB/API/IP/api_ip_remove.sql
@@ -15,6 +15,13 @@ CREATE OR REPLACE FUNCTION "api"."remove_subnet"(input_subnet cidr) RETURNS VOID
 	BEGIN
 		PERFORM api.create_log_entry('API', 'DEBUG', 'Begin api.remove_subnet');
 
+		-- Check privileges
+		IF (api.get_current_user_level() !~* 'ADMIN') THEN
+			IF (SELECT "owner" FROM "ip"."subnets" WHERE "subnet" = input_subnet) != api.get_current_user() THEN
+				RAISE EXCEPTION 'Permission to delete subnet % denied. Not owner',input_subnet;
+			END IF;
+		END IF;
+
 		-- Delete RDNS zone
 		PERFORM api.create_log_entry('API', 'INFO', 'removing rdns zone for subnet');
 		PERFORM api.remove_dns_zone(api.get_reverse_domain(input_subnet));
@@ -36,6 +43,14 @@ COMMENT ON FUNCTION "api"."remove_subnet"(cidr) IS 'Delete/deactivate an existin
 CREATE OR REPLACE FUNCTION "api"."remove_ip_range"(input_name text) RETURNS VOID AS $$
 	BEGIN
 		PERFORM api.create_log_entry('API', 'DEBUG', 'Begin api.remove_ip_range');
+
+		-- Check privileges
+		IF (api.get_current_user_level() !~* 'ADMIN') THEN
+			IF (SELECT "owner" FROM "ip"."subnets" WHERE "subnet" = 
+			(SELECT "subnet" FROM "ip"."ranges" WHERE "name" = input_range)) != api.get_current_user() THEN
+				RAISE EXCEPTION 'Permission to delete range % denied. Not owner',input_name;
+			END IF;
+		END IF;
 
 		-- Delete range		
 		PERFORM api.create_log_entry('API', 'INFO', 'Deleting range');
