227ff957bbe158454276bfb34bad0d784cf65426
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-14 05:09:21 -0700

    added metahost_modify

--- DB/API/Firewall/api_firewall_modify.sql
+++ DB/API/Firewall/api_firewall_modify.sql
@@ -26,3 +26,42 @@ CREATE OR REPLACE FUNCTION "api"."modify_firewall_default"(input_address inet, i
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."modify_firewall_default"(inet, boolean) IS 'modify an addresses default firewall action';
+
+/* API - modify_firewall_metahost
+	1) Check privileges
+	2) Check allowed fields
+	3) Update record
+*/
+CREATE OR REPLACE FUNCTION "api"."modify_firewall_metahost"(input_old_name text, input_field text, input_new_value text) RETURNS VOID AS $$
+	BEGIN
+		PERFORM api.create_log_entry('API', 'DEBUG', 'Begin api.modify_firewall_metahost');
+
+		 -- Check privileges
+		IF (api.get_current_user_level() !~* 'ADMIN') THEN
+			IF (SELECT "owner" FROM "firewall"."metahosts" WHERE "name" = input_old_name) != api.get_current_user() THEN
+				RAISE EXCEPTION 'Permission to edit metahost (%) denied. You are not owner',input_old_name;
+			END IF;
+
+			IF input_field ~* 'owner' AND input_new_value != api.get_current_user() THEN
+				RAISE EXCEPTION 'Only administrators can define a different owner (%).',input_new_value;
+			END IF;
+		END IF;
+
+		 -- Check allowed fields
+		IF input_field !~* 'name|comment|owner' THEN
+			RAISE EXCEPTION 'Invalid field % specified',input_field;
+		END IF;
+
+		-- Update record
+		PERFORM api.create_log_entry('API','INFO','update metahost');
+
+		EXECUTE 'UPDATE "firewall"."metahosts" SET ' || quote_ident($2) || ' = $3,
+		date_modified = current_timestamp, last_modifier = api.get_current_user()
+		WHERE "name" = $1'
+		USING input_old_name, input_field, input_new_value;
+
+		-- Done
+		PERFORM api.create_log_entry('API', 'DEBUG', 'finish api.modify_firewall_metahost');
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "api"."modify_firewall_metahost"(text, text, text) IS 'Modify an existing DNS TXT or SPF record';
