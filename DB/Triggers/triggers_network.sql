0c3d97d2e1a406d7c3826c021309cee49e95012c
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-16 05:42:00 -0700

    minor update

--- DB/Triggers/triggers_network.sql
+++ DB/Triggers/triggers_network.sql
@@ -25,11 +25,13 @@ CREATE OR REPLACE FUNCTION "network"."switchports_update"() RETURNS TRIGGER AS $
 		DeviceType TEXT;
 	BEGIN
 		-- Check for system types
-		SELECT "type" INTO DeviceType
-		FROM "systems"."systems"
-		WHERE "systems"."systems"."system_name" = NEW."system_name";
-		IF DeviceType !~* 'Router|Switch|Hub|Wireless Access Point' THEN
-			RAISE EXCEPTION 'Cannot create a switchport on non-network device type %',DeviceType;
+		IF NEW."system_name" != OLD."system_name" THEN
+			SELECT "type" INTO DeviceType
+			FROM "systems"."systems"
+			WHERE "systems"."systems"."system_name" = NEW."system_name";
+			IF DeviceType !~* 'Router|Switch|Hub|Wireless Access Point' THEN
+				RAISE EXCEPTION 'Cannot create a switchport on non-network device type %',DeviceType;
+			END IF;
 		END IF;
 		RETURN NEW;
 	END;
