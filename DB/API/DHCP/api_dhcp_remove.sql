b35b9006168ba80baca4960c44fff15799a538f5
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-21 10:37:44 -0700

    removed setting, added global

--- DB/API/DHCP/api_dhcp_remove.sql
+++ DB/API/DHCP/api_dhcp_remove.sql
@@ -2,8 +2,7 @@
 	1) remove_dhcp_class
 	2) remove_dhcp_class_option
 	3) remove_dhcp_subnet_option
-	4) remove_dhcp_subnet_setting
-	5) remove_dhcp_range_setting
+	4) remove_dhcp_global_option
 */
 
 /* API - remove_dhcp_class
@@ -27,7 +26,7 @@ CREATE OR REPLACE FUNCTION "api"."remove_dhcp_class"(input_class text) RETURNS V
 		PERFORM api.create_log_entry('API', 'DEBUG', 'Finish api.remove_dhcp_class');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."remove_dhcp_class"(text) IS 'Delete existing DHCP class';
+COMMENT ON FUNCTION "api"."remove_dhcp_class"(text) IS 'Delete an existing DHCP class';
 
 /* API - remove_dhcp_class_option
 	1) Check privileges
@@ -51,7 +50,7 @@ CREATE OR REPLACE FUNCTION "api"."remove_dhcp_class_option"(input_class text, in
 		PERFORM api.create_log_entry('API', 'DEBUG', 'Finish api.remove_dhcp_class_option');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."remove_dhcp_class_option"(text, text, text) IS 'Delete existing DHCP class option';
+COMMENT ON FUNCTION "api"."remove_dhcp_class_option"(text, text, text) IS 'Delete an existing DHCP class option';
 
 /* API - remove_dhcp_subnet_option
 	1) Check privileges
@@ -75,50 +74,28 @@ CREATE OR REPLACE FUNCTION "api"."remove_dhcp_subnet_option"(input_subnet cidr, 
 		PERFORM api.create_log_entry('API', 'DEBUG', 'Finish api.remove_dhcp_subnet_option');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."remove_dhcp_subnet_option"(cidr, text, text) IS 'Delete existing DHCP subnet option';
+COMMENT ON FUNCTION "api"."remove_dhcp_subnet_option"(cidr, text, text) IS 'Delete an existing DHCP subnet option';
 
-/* API - remove_dhcp_subnet_setting
+/* API - remove_dhcp_global_option
 	1) Check privileges
-	2) Remove setting
+	2) Remove global option
 */
-CREATE OR REPLACE FUNCTION "api"."remove_dhcp_subnet_setting"(input_subnet cidr, input_setting text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "api"."remove_dhcp_global_option"(input_option text, input_value text) RETURNS VOID AS $$
 	BEGIN
-		PERFORM api.create_log_entry('API','DEBUG','begin remove_dhcp_subnet_setting');
+		PERFORM api.create_log_entry('API', 'DEBUG', 'Begin api.remove_dhcp_global_option');
 
 		-- Check privileges
-		IF api.get_current_user_level() !~* 'ADMIN' THEN
-			RAISE EXCEPTION 'Permission to remove dhcp subnet setting denied for user %. Not admin.',api.get_current_user();
+		IF (api.get_current_user_level() !~* 'ADMIN') THEN
+			RAISE EXCEPTION 'Permission to remove dhcp global option denied for %. Not admin.',api.get_current_user();
 		END IF;
 
-		-- Remove setting
-		PERFORM api.create_log_entry('API','INFO','Removing DHCP subnet setting');
-		DELETE FROM "dhcp"."subnet_settings" WHERE "subnet" = input_subnet AND "setting" = input_setting;
+		-- Delete global option		
+		PERFORM api.create_log_entry('API', 'INFO', 'Deleting dhcp global option');
+		DELETE FROM "dhcp"."global_options"
+		WHERE "option" = input_option AND "value" = input_value;
 
 		-- Done
-		PERFORM api.create_log_entry('API','DEBUG','finish remove_dhcp_subnet_setting');
+		PERFORM api.create_log_entry('API', 'DEBUG', 'Finish api.remove_dhcp_global_option');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."remove_dhcp_subnet_setting"(cidr, text) IS 'Remove a DHCP subnet setting';
-
-/* API - remove_dhcp_range_setting
-	1) Check privileges
-	2) Remove setting
-*/
-CREATE OR REPLACE FUNCTION "api"."remove_dhcp_range_setting"(input_range text, input_setting text) RETURNS VOID AS $$
-	BEGIN
-		PERFORM api.create_log_entry('API','DEBUG','begin remove_dhcp_range_setting');
-
-		-- Check privileges
-		IF api.get_current_user_level() !~* 'ADMIN' THEN
-			RAISE EXCEPTION 'Permission to remove dhcp range setting denied for user %. Not admin.',api.get_current_user();
-		END IF;
-
-		-- Remove setting
-		PERFORM api.create_log_entry('API','INFO','Removing DHCP range setting');
-		DELETE FROM "dhcp"."range_settings" WHERE "name" = input_range AND "setting" = input_setting;
-
-		-- Done
-		PERFORM api.create_log_entry('API','DEBUG','finish remove_dhcp_range_setting');
-	END;
-$$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."remove_dhcp_range_setting"(text, text) IS 'Remove a DHCP subnet setting';
+COMMENT ON FUNCTION "api"."remove_dhcp_global_option"(text, text) IS 'Delete an existing DHCP global option';
