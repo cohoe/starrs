e708f3df0708999056cc96dbe93c469edf67a32d
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-21 08:35:11 -0700

    removed setting functions

--- DB/API/DHCP/api_dhcp_create.sql
+++ DB/API/DHCP/api_dhcp_create.sql
@@ -2,8 +2,7 @@
 	1) create_dhcp_class
 	2) create_dhcp_class_option
 	3) create_dhcp_subnet_option
-	4) create_dhcp_subnet_setting
-	5) create_dhcp_range_setting
+	5) create_dhcp_range_option
 	6) create_dhcp_global_option
 */
 
@@ -84,48 +83,18 @@ CREATE OR REPLACE FUNCTION "api"."create_dhcp_subnet_option"(input_subnet cidr, 
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."create_dhcp_subnet_option"(cidr, text, text) IS 'Create DHCP subnet option';
 
-/* API - create_dhcp_subnet_setting
-	1) Check privileges
-	2) Check subnet DHCP-ness
-	3) Create setting
-*/
-CREATE OR REPLACE FUNCTION "api"."create_dhcp_subnet_setting"(input_subnet cidr, input_setting text, input_value text) RETURNS VOID AS $$
-	BEGIN
-		PERFORM api.create_log_entry('API','DEBUG','begin create_dhcp_subnet_setting');
-
-		-- Check privileges
-		IF api.get_current_user_level() !~* 'ADMIN' THEN
-			RAISE EXCEPTION 'Permission to create dhcp subnet setting denied for user %. You are not admin.',api.get_current_user();
-		END IF;
-
-		-- Check if subnet is marked for DHCP
-		IF (SELECT "dhcp_enable" FROM "ip"."subnets" WHERE "subnet" = input_subnet) IS FALSE THEN
-			RAISE EXCEPTION 'Subnet % is not marked for DHCP configuration',input_subnet;
-		END IF;
-
-		-- Create setting
-		PERFORM api.create_log_entry('API','INFO','Creating new DHCP subnet setting');
-		INSERT INTO "dhcp"."subnet_settings" ("subnet","setting","value","last_modifier")
-		VALUES (input_subnet, input_setting, input_value, api.get_current_user());
-
-		-- Done
-		PERFORM api.create_log_entry('API','DEBUG','finish create_dhcp_subnet_setting');
-	END;
-$$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."create_dhcp_subnet_setting"(cidr, text, text) IS 'Create a DHCP subnet setting';
-
-/* API - create_dhcp_range_setting
+/* API - create_dhcp_range_option
 	1) Check privileges
 	2) Check for range DHCPness
-	3) Create setting
+	3) Create option
 */
-CREATE OR REPLACE FUNCTION "api"."create_dhcp_range_setting"(input_range text, input_setting text, input_value text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "api"."create_dhcp_range_option"(input_range text, input_option text, input_value text) RETURNS VOID AS $$
 	BEGIN
-		PERFORM api.create_log_entry('API','DEBUG','begin create_dhcp_range_setting');
+		PERFORM api.create_log_entry('API','DEBUG','begin create_dhcp_range_option');
 
 		-- Check privileges
 		IF api.get_current_user_level() !~* 'ADMIN' THEN
-			RAISE EXCEPTION 'Permission to create dhcp subnet setting denied for user %. You are not admin.',api.get_current_user();
+			RAISE EXCEPTION 'Permission to create dhcp range option denied for user %. You are not admin.',api.get_current_user();
 		END IF;
 
 		-- Check if range is marked for DHCP
@@ -133,16 +102,16 @@ CREATE OR REPLACE FUNCTION "api"."create_dhcp_range_setting"(input_range text, i
 			RAISE EXCEPTION 'Range % is not marked for DHCP configuration',input_range;
 		END IF;
 
-		-- Create setting
-		PERFORM api.create_log_entry('API','INFO','Creating new DHCP range setting');
-		INSERT INTO "dhcp"."range_settings" ("name","setting","value","last_modifier")
-		VALUES (input_range, input_setting, input_value, api.get_current_user());
+		-- Create option
+		PERFORM api.create_log_entry('API','INFO','Creating new DHCP range option');
+		INSERT INTO "dhcp"."range_options" ("name","option","value","last_modifier")
+		VALUES (input_range, input_option, input_value, api.get_current_user());
 
 		-- Done
-		PERFORM api.create_log_entry('API','DEBUG','finish create_dhcp_range_setting');
+		PERFORM api.create_log_entry('API','DEBUG','finish create_dhcp_range_option');
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."create_dhcp_range_setting"(text, text, text) IS 'Create a DHCP range setting';
+COMMENT ON FUNCTION "api"."create_dhcp_range_option"(text, text, text) IS 'Create a DHCP range option';
 
 /* API - create_dhcp_global_option
 	1) Check privileges
