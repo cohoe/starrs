1fa260f18b4f1f5bdef99313f26ce16422fa9af4
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-22 07:05:17 -0700

    changed function inserts to pull in definition and comments

--- DB/Setup/Document.sql
+++ DB/Setup/Document.sql
@@ -1,9 +1,10 @@
 /* Document.sql */
 
-INSERT INTO "documentation"."functions" ("specific_name","returns","name","definition") (
-SELECT "specific_name","data_type","routine_name",api.get_function_definition("specific_name")
-FROM "information_schema"."routines"
-WHERE "specific_schema" = 'api');
+INSERT INTO "documentation"."functions" ("specific_name","returns","name","definition","comment") (
+SELECT "specific_name","data_type","routine_name",regexp_replace(pg_get_functiondef(('api.'||"routine_name")::regproc::oid),E'^(CREATE OR REPLACE FUNCTION )(.*)\n RETURNS(.*)',E'\\2') AS "definition", obj_description(pg_proc.oid) AS "comment" 
+FROM information_schema.routines 
+JOIN pg_proc ON information_schema.routines.routine_name = pg_proc.proname 
+WHERE specific_schema = 'api');
 
 INSERT INTO "documentation"."arguments" ("specific_name","argument","type","position") (
 SELECT "specific_name","parameter_name","udt_name","ordinal_position"
