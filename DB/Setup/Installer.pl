4e32bd2dcf926390eaa473bd18fd00b26824f2a5
Grant Cohoe <cohoe@csh.rit.edu>
2011-06-22 07:59:58 -0700

    added host, port, user customization

--- DB/Setup/Installer.pl
+++ DB/Setup/Installer.pl
@@ -4,6 +4,9 @@ use strict;
 use warnings;
 use Cwd;
 
+my $dbhost = "localhost";
+my $dbuser = "impulse_admin";
+my $dbport = 5432;
 my $dbname = "impulse";
 
 my @files = ('create','remove','get','modify','utility');
@@ -12,51 +15,51 @@ my @schemas = ('dhcp','dns','firewall','ip','management','network','systems');
 my $dir = getcwd();
 
 print "Jumpstarting...\n";
-system("psql","-f","\"$dir/Setup/Jumpstart.sql\"");
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Jumpstart.sql\"");
 print "Setup...\n";
-system("psql","-f","\"$dir/Setup/Setup.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Setup.sql\"",$dbname);
 
 print "Types...\n";
-system("psql","-f","\"$dir/Setup/Types.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Types.sql\"",$dbname);
 
 foreach my $schema (@schemas)
 {
 	foreach my $file (@files)
 	{
 		print "API $schema $file\n";
-		system("psql","-f","\"$dir/API/$schema/api\_$schema\_$file.sql\"",$dbname);
+		system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/API/$schema/api\_$schema\_$file.sql\"",$dbname);
 	}
 }
 
 print "Tables...\n";
-system("psql","-f","\"$dir/Setup/Tables.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Tables.sql\"",$dbname);
 print "Fixes...\n";
-system("psql","-f","\"$dir/Setup/Fixes.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Fixes.sql\"",$dbname);
 print "Constraints...\n";
-system("psql","-f","\"$dir/Setup/Constraints.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Constraints.sql\"",$dbname);
 
 foreach my $schema (@schemas)
 {
 	print "Triggers for $schema\n";
-	system("psql","-f","\"$dir/Triggers/triggers\_$schema.sql\"",$dbname);
+	system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Triggers/triggers\_$schema.sql\"",$dbname);
 }
 
 print "Triggers...\n";
-system("psql","-f","\"$dir/Triggers/Triggers.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Triggers/Triggers.sql\"",$dbname);
 print "Views...\n";
-system("psql","-f","\"$dir/Setup/Views.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Views.sql\"",$dbname);
 print "Base...\n";
-system("psql","-f","\"$dir/Setup/Base.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Base.sql\"",$dbname);
 print "Privileges...\n";
-system("psql","-f","\"$dir/Setup/Privileges.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Privileges.sql\"",$dbname);
 print "Documentation...\n";
-system("psql","-f","\"$dir/API/Documentation/api_documentation_get.sql\"",$dbname);
-system("psql","-f","\"$dir/Setup/Document.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/API/Documentation/api_documentation_get.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/Setup/Document.sql\"",$dbname);
 foreach my $schema (@schemas)
 {
 	print "Documentation for $schema\n";
-	system("psql","-f","\"$dir/API/Documentation/api\_documentation\_$schema.sql\"",$dbname);
+	system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/API/Documentation/api\_documentation\_$schema.sql\"",$dbname);
 }
 print "Documentation for documentation\n";
-system("psql","-f","\"$dir/API/Documentation/api\_documentation\_documentation.sql\"",$dbname);
+system("psql","-h",$dbhost,"-p",$dbport,"-U",$dbuser,"-f","\"$dir/API/Documentation/api\_documentation\_documentation.sql\"",$dbname);
 print "Done!";
