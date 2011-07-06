bd3957af516de276a34a0438b0d3a084f7865c9b
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-06 18:05:38 -0400

    updated installer

--- DB/Setup/Installer.pl
+++ DB/Setup/Installer.pl
@@ -10,62 +10,64 @@ my $dbport = 5432;
 my $dbname = "impulse";
 
 my @files = ('create','remove','get','modify','utility');
-my @schemas = ('dhcp','dns','firewall','ip','management','network','systems');
+my @schemas = ('DHCP','DNS','Firewall','IP','Management','Network','Systems');
 
 my $dir = getcwd();
 
 
 print "Jumpstarting...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Jumpstart.sql\"");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Jumpstart.sql\"");
 print "Setup...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Setup.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Setup.sql\" $dbname");
 print "Types...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Types.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Types.sql\" $dbname");
 
 foreach my $schema (@schemas)
 {
 	foreach my $file (@files)
 	{
 		print "API $schema $file\n";
-		system("psql -h $dbhost -p $dbport -U $dbuser -f \"API\\$schema\\api\_$schema\_$file.sql\" $dbname");
+		system("psql -p $dbport -U $dbuser -f \"API\/$schema\/api\_".lc($schema)."\_$file.sql\" $dbname");
 
 	}
 }
 
 print "Tables...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Tables.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Tables.sql\" $dbname");
 print "Fixes...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Fixes.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Fixes.sql\" $dbname");
 print "Constraints...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Constraints.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Constraints.sql\" $dbname");
 
 foreach my $schema (@schemas)
 {
 	print "Triggers for $schema\n";
-	system("psql -h $dbhost -p $dbport -U $dbuser -f \"Triggers\\triggers_$schema.sql\" $dbname");
+	$schema = lc($schema);
+	system("psql -p $dbport -U $dbuser -f \"Triggers\/triggers_$schema.sql\" $dbname");
 }
 
 print "Triggers...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Triggers\\Triggers.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Triggers\/Triggers.sql\" $dbname");
 
 print "Views...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Views.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Views.sql\" $dbname");
 print "Base...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Base.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Base.sql\" $dbname");
 print "Privileges...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Privileges.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Privileges.sql\" $dbname");
 
 print "Documentation...\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"API\\Documentation\\api_documentation_get.sql\" $dbname");
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"API\\Documentation\\api_documentation_utility.sql\" $dbname");
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"Setup\\Document.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"API\/Documentation\/api_documentation_get.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"API\/Documentation\/api_documentation_utility.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"Setup\/Document.sql\" $dbname");
 foreach my $schema (@schemas)
 {
 	print "Documentation for $schema\n";
-	system("psql -h $dbhost -p $dbport -U $dbuser -f \"API\\Documentation\\api_documentation_$schema.sql\" $dbname");
+	$schema = lc($schema);
+	system("psql -p $dbport -U $dbuser -f \"API\/Documentation\/api_documentation_$schema.sql\" $dbname");
 
 }
 print "Documentation for documentation\n";
-system("psql -h $dbhost -p $dbport -U $dbuser -f \"API\\Documentation\\api_documentation_documentation.sql\" $dbname");
+system("psql -p $dbport -U $dbuser -f \"API\/Documentation\/api_documentation_documentation.sql\" $dbname");
 
-print "Done!";
+print "Done!\n";
