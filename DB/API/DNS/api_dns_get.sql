836618df87a1f6b7f27c1b42d7708d6538430687
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-11 05:50:59 -0700

    Edited DB/API/DNS/api_dns_get.sql via GitHub

--- DB/API/DNS/api_dns_get.sql
+++ DB/API/DNS/api_dns_get.sql
@@ -25,14 +25,14 @@ CREATE OR REPLACE FUNCTION "api"."get_dns_txt"(input_address inet) RETURNS SETOF
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_txt"(inet) IS 'Get all DNS TXT records for an address';
 
-/* API - get_dns_pointer */
-CREATE OR REPLACE FUNCTION "api"."get_dns_pointer"(input_address inet) RETURNS SETOF "dns"."pointer_data" AS $$
+/* API - get_dns_pointers */
+CREATE OR REPLACE FUNCTION "api"."get_dns_pointers"(input_address inet) RETURNS SETOF "dns"."pointer_data" AS $$
 	BEGIN
 		RETURN QUERY (SELECT "alias","hostname","zone","address","type","extra","ttl","owner","date_created","date_modified","last_modifier"
 			FROM "dns"."pointers" WHERE "address" = input_address);
 	END;
 $$ LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION "api"."get_dns_pointer"(inet) IS 'Get all DNS pointer (SRV,CNAME) records for an address';
+COMMENT ON FUNCTION "api"."get_dns_pointers"(inet) IS 'Get all DNS pointer (SRV,CNAME) records for an address';
 
 /* API - get_dns_a */
 CREATE OR REPLACE FUNCTION "api"."get_dns_a"(input_address inet) RETURNS SETOF "dns"."a_data" AS $$
