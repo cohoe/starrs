1158b186b255b47fc681b0ec309b6ba32d9ecf85
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-11 06:03:07 -0700

    added order by's

--- DB/API/DNS/api_dns_get.sql
+++ DB/API/DNS/api_dns_get.sql
@@ -20,7 +20,7 @@ COMMENT ON FUNCTION "api"."get_dns_ns"(inet) IS 'Get all data pertanent to DNS N
 CREATE OR REPLACE FUNCTION "api"."get_dns_txt"(input_address inet) RETURNS SETOF "dns"."txt_data" AS $$
 	BEGIN
 		RETURN QUERY (SELECT "hostname","zone","address","type","text","ttl","owner","date_created","date_modified","last_modifier"
-			FROM "dns"."txt" WHERE "address" = input_address);
+			FROM "dns"."txt" WHERE "address" = input_address ORDER BY "hostname" ASC);
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_txt"(inet) IS 'Get all DNS TXT records for an address';
@@ -29,7 +29,7 @@ COMMENT ON FUNCTION "api"."get_dns_txt"(inet) IS 'Get all DNS TXT records for an
 CREATE OR REPLACE FUNCTION "api"."get_dns_pointers"(input_address inet) RETURNS SETOF "dns"."pointer_data" AS $$
 	BEGIN
 		RETURN QUERY (SELECT "alias","hostname","zone","address","type","extra","ttl","owner","date_created","date_modified","last_modifier"
-			FROM "dns"."pointers" WHERE "address" = input_address);
+			FROM "dns"."pointers" WHERE "address" = input_address ORDER BY "alias" ASC);
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_pointers"(inet) IS 'Get all DNS pointer (SRV,CNAME) records for an address';
