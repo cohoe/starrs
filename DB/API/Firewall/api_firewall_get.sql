0ef1e2343a9c4caa6a9b3d7cba445bd8ba0cd130
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-11 04:50:25 -0700

    Edited DB/API/Firewall/api_firewall_get.sql via GitHub

--- DB/API/Firewall/api_firewall_get.sql
+++ DB/API/Firewall/api_firewall_get.sql
@@ -5,3 +5,11 @@ CREATE OR REPLACE FUNCTION "api"."get_firewall_default"(input_address inet) RETU
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_firewall_default"(inet) IS 'Get the default firewall action of an address';
+
+/* API - get_firewall_program_name */
+CREATE OR REPLACE FUNCTION "api"."get_firewall_program_name"(input_port integer) RETURNS text AS $$
+	BEGIN
+		RETURN (SELECT "name" FROM "firewall"."programs" WHERE "port" = input_port);
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "api"."get_firewall_program_name"(integer) IS 'Get the name of a preconfigured firewall program';
