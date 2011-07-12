15ab8c3c8cebe09b5afa7f64734c4a40dad22e90
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-12 18:48:27 -0400

    fixed default view

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,44 +2,15 @@
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-		// Header
-		$info['header'] = $this->load->view('mockup/header',"",TRUE);
-		
-		// Sidebar
-		$info['sidebar'] = $this->load->view('mockup/sidebar',"",TRUE);
-		
-		// Information
-		$navbarData['options'] = array('Create System','Create Interface','Assign IP Address');
-		$navbarData['systemName'] = "Getting Started";
-		$navbarData['edit'] = false;
-		$info['navbar'] = $this->load->view('mockup/navbar',$navbarData,TRUE);
-		$info['data'] = $this->load->view('mockup/getstarted',"",TRUE);
-		
-		// Load the main view
-		$this->load->view('mockup/main',$info);
+		$this->_load_get_started();
 	}
 
     private function _load_get_started() {
 
 		// Information
-		$navbar = new Navbar("Getting Started",FALSE,FALSE,NULL,"/",array("Create System"));
+		$navbar = new Navbar("Getting Started",FALSE,FALSE,NULL,"/systems",array("Create System"=>'create/system'));
 
 		// Load view data
 		$info['header'] = $this->load->view('core/header',"",TRUE);
@@ -49,7 +20,7 @@ class Welcome extends CI_Controller {
 		$info['title'] = "Getting Started";
 
 		// Load the main view
-		$this->load->view('mockup/main',$info);
+		$this->load->view('core/main',$info);
 	}
 }
 
