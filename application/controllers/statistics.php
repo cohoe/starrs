7b066b064e643986e011ca2539658aa8a0cc13b3
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-13 17:13:34 -0400

    fixed statistics displays

--- application/controllers/statistics.php
+++ application/controllers/statistics.php
@@ -4,9 +4,9 @@ class Statistics extends CI_Controller {
 	public function index() {
 		
 		// Information
-		$navOptions = array("OS Distribution"=>'os_distribution',"OS Family Distribution"=>'os_family_distribution');
-		$navbar = new Navbar("Statistics",FALSE,FALSE,FALSE,NULL,"/statistics",$navOptions);
-		
+        $navOptions = array("OS Distribution"=>'/statistics/os_distribution',"OS Family Distribution"=>'/statistics/os_family_distribution');
+		$navbar = new Navbar("Statistics", null, $navOptions);
+        
 		// Load view data
 		$info['header'] = $this->load->view('core/header',"",TRUE);
 		$info['sidebar'] = $this->load->view('core/sidebar',"",TRUE);
@@ -22,8 +22,8 @@ class Statistics extends CI_Controller {
 		$data = $this->api->systems->get_os_distribution();
 		
 		// Information
-		$navOptions = array("OS Distribution"=>'os_distribution',"OS Family Distribution"=>'os_family_distribution');
-		$navbar = new Navbar("OS Distribution",FALSE,FALSE,FALSE,NULL,"/statistics",$navOptions);
+		$navOptions = array("OS Distribution"=>'/statistics/os_distribution',"OS Family Distribution"=>'/statistics/os_family_distribution');
+		$navbar = new Navbar("Statistics", null, $navOptions);
 		
 		// Load view data
 		$info['header'] = $this->load->view('core/header',"",TRUE);
@@ -40,8 +40,8 @@ class Statistics extends CI_Controller {
 		$data = $this->api->systems->get_os_family_distribution();
 		
 		// Information
-		$navOptions = array("OS Distribution"=>'os_distribution',"OS Family Distribution"=>'os_family_distribution');
-		$navbar = new Navbar("OS Family Distribution",FALSE,FALSE,FALSE,NULL,"/statistics",$navOptions);
+		$navOptions = array("OS Distribution"=>'/statistics/os_distribution',"OS Family Distribution"=>'/statistics/os_family_distribution');
+		$navbar = new Navbar("Statistics", null, $navOptions);
 		
 		// Load view data
 		$info['header'] = $this->load->view('core/header',"",TRUE);
