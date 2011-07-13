f769656f390ff7ef545bebd29483b3cf7d6b641e
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-13 16:36:24 -0400

    something

--- application/views/systems/system.php
+++ application/views/systems/system.php
@@ -5,6 +5,7 @@
 		<tr><td><em>Owner:</em></td><td><?echo $system->get_owner();?></td></tr>
 		<tr><td><em>Type:</em></td><td><?echo $system->get_type();?></td></tr>
 		<tr><td><em>OS:</em></td><td><?echo $system->get_os_name();?></td></tr>
+		<tr><td><em>Comment:</em></td><td><?echo $system->get_comment();?></td></tr>
 		<tr><td><em>Date Created:</em></td><td><?echo $system->get_date_created();?></td></tr>
 		<tr><td><em>Date Modified:</em></td><td><?echo $system->get_date_modified();?></td></tr>
 		<tr><td><em>Last Modifier:</em></td><td><?echo $system->get_last_modifier();?></td></tr>
