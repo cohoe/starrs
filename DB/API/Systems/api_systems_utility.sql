c3ebc0c620a43c8d97acd2a91301f99c02e688b0
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-14 06:19:09 -0700

    Edited DB/API/Systems/api_systems_utility.sql via GitHub

--- DB/API/Systems/api_systems_utility.sql
+++ DB/API/Systems/api_systems_utility.sql
@@ -8,3 +8,17 @@ CREATE OR REPLACE FUNCTION "api"."get_interface_address_owner"(input_address ine
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_interface_address_owner"(inet) IS 'Get the owner of an interface address';
+
+CREATE OR REPLACE FUNCTION "api"."renew_system"(input_system_name text) RETURNS VOID AS $$
+	BEGIN
+		PERFORM api.create_log_entry('API','DEBUG','begin api.renew_system');
+
+		PERFORM api.create_log_entry('API','INFO','updating system'|input_system_name);
+		UPDATE "systems"."systems"
+		SET "renew_date" = date(current_date + interval '1 year')
+		WHERE "system_name" = input_system_name;
+
+		PERFORM api.create_log_entry('API','DEBUG','finish api.renew_system');
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "api"."renew_system"(text) IS 'renew a system registration for another year';
