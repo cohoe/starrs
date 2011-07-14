8743b485d33fcf3aa4ea6777da727bb2e73ac06f
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-14 05:31:17 -0700

    added check for blank line in validate

--- DB/API/Management/api_management_utility.sql
+++ DB/API/Management/api_management_utility.sql
@@ -33,6 +33,9 @@ CREATE OR REPLACE FUNCTION "api"."validate_name"(input text) RETURNS TEXT AS $$
 		IF BadCrap != '' THEN
 			RAISE EXCEPTION 'Invalid characters detected in string "%"',input;
 		END IF;
+		IF input = "" THEN
+			RAISE EXCEPTION 'Name cannot be blank';
+		END IF;
 		RETURN input;
 	END;
 $$ LANGUAGE 'plpgsql';
