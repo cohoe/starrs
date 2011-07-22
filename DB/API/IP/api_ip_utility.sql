8db71d5b5573c0aedce672ed5a7240da53b87b1f
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-22 11:23:30 -0700

    Edited DB/API/IP/api_ip_utility.sql via GitHub

--- DB/API/IP/api_ip_utility.sql
+++ DB/API/IP/api_ip_utility.sql
@@ -5,3 +5,15 @@ CREATE OR REPLACE FUNCTION "api"."ip_arp"(input_address inet) RETURNS macaddr AS
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."ip_arp"(inet) IS 'Get the MAC address assiciated with an IP address';
+
+/* API - ip_in_subnet */
+CREATE OR REPLACE FUNCTION "api"."ip_in_subnet"(input_address inet, input_subnet cidr) RETURNS BOOLEAN AS $$
+	BEGIN
+		IF input_address << input_subnet THEN
+			RETURN TRUE;
+		ELSE
+			RETURN FALSE;
+		END IF;
+	END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION "api"."ip_in_subnet"(inet, cidr) IS 'True or False if an address is contained within a given subnet';
