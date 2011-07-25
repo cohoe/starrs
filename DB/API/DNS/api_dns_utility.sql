b21e02b5f1134ddaf6277bf75a45056ed5cf9275
Grant Cohoe <cohoe@csh.rit.edu>
2011-07-25 11:40:37 -0700

    Edited DB/API/DNS/api_dns_utility.sql via GitHub

--- DB/API/DNS/api_dns_utility.sql
+++ DB/API/DNS/api_dns_utility.sql
@@ -108,12 +108,14 @@ COMMENT ON FUNCTION "api"."dns_resolve"(text, text, integer) IS 'Resolve a hostn
 
 /* API - nsupdate */
 CREATE OR REPLACE FUNCTION "api"."nsupdate"() RETURNS TEXT AS $$
-	my $command = "";
 	my $sth = spi_query("SELECT * FROM dns.queue");
+	my $result = $sth->{status};
 	while (defined ($row = spi_fetchrow($sth))) {
-		$command .= "UPDATE $row->{directive} $row->{hostname}.$row->{zone} $row->{ttl} $row->{type} $row->{extra} $row->{target} \n";
+		$result .= "Hello";
+		my $command .= "UPDATE $row->{directive} $row->{hostname}.$row->{zone} $row->{ttl} $row->{type} $row->{extra} $row->{target} \nsend";
+		$result .= `echo "$command" | nsupdate 2>&1`;
 	}
-	my $result = `echo "$command" | nsupdate 2>&1`;
+	
 	#my $result = $command;
 	return $result;
 $$ LANGUAGE 'plperlu';
