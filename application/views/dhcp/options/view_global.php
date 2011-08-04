22462a5307e3d9e24a3a191d045eda0c1186aee4
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-04 19:42:47 -0400

    added styling

--- application/views/dhcp/options/view_global.php
+++ application/views/dhcp/options/view_global.php
@@ -1,8 +1,8 @@
 <div class="item_container">
-    <table>
+    <table style="border: 1px solid black">
         <tr><th>Option</th><th>Value</th></tr>
         <? foreach ($options as $option) {
-            echo '<tr><td style="text-align:right;">'.$option->get_option().'</td><td style="text-align:left;">'.$option->get_value().'</tr>';
+            echo '<tr><td style="text-align:right; margin-right: .5em;">'.$option->get_option().'</td><td style="text-align:left; margin-left: .5em;">'.$option->get_value().'</tr>';
         } ?>
     </table>
 </div>
