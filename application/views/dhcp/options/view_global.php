af74ae82a728640d313c4386ea83f152af64ade5
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-04 19:46:37 -0400

    more styles

--- application/views/dhcp/options/view_global.php
+++ application/views/dhcp/options/view_global.php
@@ -1,8 +1,8 @@
 <div class="item_container">
     <table style="border: 1px solid black; margin-left: auto; margin-right: auto;">
-        <tr style="border-bottom: 1px solid black;"><th>Option</th><th>Value</th></tr>
+        <tr><th>Option</th><th>Value</th></tr>
         <? foreach ($options as $option) {
-            echo '<tr><td style="text-align:right; padding-right: .5em;">'.$option->get_option().'</td><td style="text-align:left; padding-left: .5em;">'.$option->get_value().'</tr>';
+            echo '<tr><td style="text-align:right; padding-right: .5em; padding-left: .1em;">'.$option->get_option().'</td><td style="text-align:left; padding-left: .5em; padding-right: .1em;">'.$option->get_value().'</tr>';
         } ?>
     </table>
 </div>
