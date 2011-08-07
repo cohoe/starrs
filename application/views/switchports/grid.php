432f36131d1ff5b2e4a3923eb9fefff99c0f5f8c
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-06 20:37:03 -0400

    grid view development

--- application/views/switchports/grid.php
+++ application/views/switchports/grid.php
@@ -1,5 +1,5 @@
 <div class="item_container">
     <? foreach ($sPorts as $sPort) {?>
-        <div style="border: 1px solid black; margin: 1px; width: 48px; height: 48px;"><?echo $sPort->get_port_name();?></div>
+        <div style="border: 1px solid black; margin: 1px; width: 48px; height: 48px; float: left;"><?echo $sPort->get_port_name();?></div>
     <?} ?>
 </div>
