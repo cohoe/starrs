17a96f71922c621cccab80111305f8a48962135c
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-07 15:09:52 -0400

    added api call

--- application/models/API/Network/api_network_modify.php
+++ application/models/API/Network/api_network_modify.php
@@ -18,6 +18,19 @@ class Api_network_modify extends ImpulseModel {
 		// Check errors
         $this->_check_error($query);
 	}
+
+    public function switchview_settings($systemName, $field, $newValue) {
+        // SQL Query
+		$sql = "SELECT api.modify_system_switchview(
+		    {$this->db->escape($systemName)},
+		    {$this->db->escape($field)},
+		    {$this->db->escape($newValue)}
+		)";
+		$query = $this->db->query($sql);
+
+		// Check errors
+        $this->_check_error($query);
+    }
 }
 /* End of file api_network_modify.php */
 /* Location: ./application/models/API/Network/api_network_modify.php */
