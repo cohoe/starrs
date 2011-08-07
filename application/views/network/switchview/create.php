7006c68cf5345dcda6b686d7146a15505a2bfebf
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-07 12:54:22 -0400

    added create

--- application/views/network/switchview/create.php
+++ application/views/network/switchview/create.php
@@ -1,6 +1,7 @@
 <div class="item_container">
 	<form method="POST" class="input_form">
-	    <label for="community">SNMP Community: </label><input type="text" name="community" class="input_form_input" /><br />
+	    <label for="ro_community">SNMP RO Community: </label><input type="text" name="ro_community" class="input_form_input" /><br />
+        <label for="rw_community">SNMP RW Community: </label><input type="text" name="rw_community" class="input_form_input" /><br />
 		<label for="enable">Enable?: </label>
 		<input type="radio" name="enable" value="t" class="input_form_radio" checked />Yes
 		<input type="radio" name="enable" value="f" class="input_form_radio" />No
