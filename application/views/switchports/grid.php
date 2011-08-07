f16912e031db5021fd147b3363700d8acd5706fb
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-06 20:56:06 -0400

    derp

--- application/views/switchports/grid.php
+++ application/views/switchports/grid.php
@@ -4,20 +4,21 @@
 		$i = 0;
 		$cells_per_row = 10;
 		$class = "firewall_rule_port_box_unknown";
-        if($sPort->get_state() == 't') {
-            $class = "firewall_rule_port_box_allow";
-        }
-        elseif($sPort->get_state() == 'f') {
-            $class = "firewall_rule_port_box_deny";
-        }
-        else {
-            $class = "firewall_rule_port_box_unknown";
-        }
 
 		foreach ($sPorts as $sPort) {
 			if($i % $cells_per_row == 0) {
 				echo "<tr>";
 			}
+            if($sPort->get_state() == 't') {
+               $class = "firewall_rule_port_box_allow";
+            }
+            elseif($sPort->get_state() == 'f') {
+                $class = "firewall_rule_port_box_deny";
+            }
+            else {
+                $class = "firewall_rule_port_box_unknown";
+            }
+
 			echo "<td class=\"$class\"><a href=\"/switchport/view/".rawurlencode($sPort->get_system_name())."/".rawurlencode($sPort->get_port_name())."\">".htmlentities($sPort->get_port_name())."</a></td>";
 			if(($i+1) % $cells_per_row == 0) {
 				echo "</tr>";
