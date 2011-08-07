013340fde9a14002c57a5e69ef79473bf1e6ef57
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-07 12:44:33 -0400

    edit view

--- application/views/network/switchview/edit.php
+++ application/views/network/switchview/edit.php
@@ -1,6 +1,7 @@
 <div class="item_container">
 	<form method="POST" class="input_form">
-	    <label for="community">SNMP Community: </label><input type="text" name="community" value="<?echo $settings['snmp_community'];?>" class="input_form_input" /><br />
+	    <label for="community">SNMP RO Community: </label><input type="text" name="ro_community" value="<?echo $settings['ro_community'];?>" class="input_form_input" /><br />
+        <label for="community">SNMP RW Community: </label><input type="text" name="rw_community" value="<?echo $settings['rw_community'];?>" class="input_form_input" /><br />
 		<label for="enable">Enable?: </label>
 		<input type="radio" name="enable" value="t" class="input_form_radio" <?echo ($settings['enable']=='t')?"checked":""?> />Yes
 		<input type="radio" name="enable" value="f" class="input_form_radio" <?echo ($settings['enable']=='f')?"checked":""?> />No
