c9050fde8b3c6494722df6f9016a4e98e145c5e4
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-07 19:40:39 -0400

    derp

--- application/views/interfaces/view.php
+++ application/views/interfaces/view.php
@@ -1,10 +1,10 @@
 <div class="item_container">
     <table class="item_information_area_table">
-        <tr><td><em>Name:</em></td><td><?echo $interface->get_interface_name();?></td></tr>
-        <tr><td><em>MAC:</em></td><td><?echo $interface->get_mac();?></td></tr>
-        <tr><td><em>Comment:</em></td><td><?echo $interface->get_comment();?></td></tr>
+        <tr><td><em>Name:</em></td><td><?echo $int->get_interface_name();?></td></tr>
+        <tr><td><em>MAC:</em></td><td><?echo $int->get_mac();?></td></tr>
+        <tr><td><em>Comment:</em></td><td><?echo $int->get_comment();?></td></tr>
     </table>
     <div class="infobar">
-        <span class="infobar_text">Created on <?echo $interface->get_date_created();?> - Modified by <?echo $interface->get_last_modifier();?> on <?echo $interface->get_date_modified();?></span>
+        <span class="infobar_text">Created on <?echo $int->get_date_created();?> - Modified by <?echo $int->get_last_modifier();?> on <?echo $int->get_date_modified();?></span>
     </div>
 </div>
