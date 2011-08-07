431d0e43ab7d67565648840f1e9562a6b0d33d50
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-07 13:53:27 -0400

    added edit data

--- application/views/switchport/edit.php
+++ application/views/switchport/edit.php
@@ -1,17 +1,23 @@
 <div class="item_information_area">
     <form method="POST" class="input_form">
-        <label for="system_name">System Name: </label><input type="text" name="system_name" class="input_form_input" readonly /><br />
-        <label for="port_name">Port Name: </label><input type="text" name="port_name" class="input_form_input" /><br />
+        <label for="system_name">System Name: </label><input type="text" name="system_name" value="<?echo $sPort->get_system_name();?>" class="input_form_input" readonly /><br />
+        <label for="port_name">Port Name: </label><input type="text" name="port_name" value="<?echo $sPort->get_port_name();?>" class="input_form_input" /><br />
         <label for="type">Type: </label>
 		<select name="type" class="input_form_input">
 			<? foreach ($types as $type) {
-				echo "<option value=\"".$type."\">".$type."</option>";
+                if($sPort->get_type() == $type) {
+                    echo "<option value=\"".$type."\" selected>".$type."</option>";
+                }
+                else {
+				    echo "<option value=\"".$type."\">".$type."</option>";
+                }
 			} ?>
 		</select><br />
-        <label for="description">Description: </label><input type="text" name="description" class="input_form_input" /><br />
+        <label for="description">Description: </label><input type="text" name="description" value="<?echo $sPort->get_description();?>" class="input_form_input" /><br />
         <label for="enable">Enable?: </label>
 		<input type="radio" name="enable" value="t" class="input_form_radio" <?echo ($sys->get_switchview_enable()=='t')?"checked":""?> />Yes
 		<input type="radio" name="enable" value="f" class="input_form_radio" <?echo ($sys->get_switchview_enable()=='f')?"checked":""?> />No
+        <br />
         <label for="submit">&nbsp;</label><input type="submit" name="submit" value="Create" class="input_form_submit"/>
 	</form>
 </div>
