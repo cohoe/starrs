d78ca7fff2bcfa03311b0c1e3ef2e1442cae3867
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-07 15:29:10 -0400

    added port link

--- application/controllers/switchports.php
+++ application/controllers/switchports.php
@@ -22,6 +22,7 @@ class Switchports extends ImpulseController {
         // Navbar
         $navModes['CREATE'] = "/switchports/create/".rawurlencode(self::$sys->get_system_name());
         $navOptions['System'] = "/systems/view/".rawurlencode(self::$sys->get_system_name());
+        $navOptions['Switchview'] = '/switchview/settings/'.rawurlencode(self::$sys->get_system_name());
         $navbar = new Navbar("Switchports on ".self::$sys->get_system_name(), $navModes, $navOptions);
 
         // Load view data
