5d28b8690cb40e5a91627d9101fbc7658e9bce0a
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-07 00:56:42 -0400

    derp

--- application/views/network/switchview/create.php
+++ application/views/network/switchview/create.php
@@ -4,6 +4,7 @@
 		<label for="enable">Enable?: </label>
 		<input type="radio" name="enable" value="t" class="input_form_radio" checked />Yes
 		<input type="radio" name="enable" value="f" class="input_form_radio" />No
+        <br />
 		<label for="submit">&nbsp;</label><input type="submit" name="submit" value="Create" class="input_form_submit"/>
 	</form>
 </div>
