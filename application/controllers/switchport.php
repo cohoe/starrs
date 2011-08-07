546dc548dc88003895586781633001059c3f50b8
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-07 14:16:09 -0400

    debug

--- application/controllers/switchport.php
+++ application/controllers/switchport.php
@@ -67,7 +67,7 @@ class Switchport extends ImpulseController {
         $this->_load_system(rawurldecode($systemName));
         $this->_load_switchports(rawurldecode($systemName));
         try {
-            exit("$portName");
+            exit(rawurldecode($portName));
             self::$sPort = self::$sys->get_switchport(rawurldecode($portName));
         }
         catch(ObjectException $oE) {
