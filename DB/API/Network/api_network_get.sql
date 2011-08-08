a3001acdde72364071617ee7b0cc023f883a37e3
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-08 16:12:07 -0300

    added get_switchview_descriptions

--- DB/API/Network/api_network_get.sql
+++ DB/API/Network/api_network_get.sql
@@ -293,3 +293,65 @@ CREATE OR REPLACE FUNCTION "api"."get_network_switchport"(input_system_name text
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_network_switchport"(text,text) IS 'Get a switchport';
+
+CREATE OR REPLACE FUNCTION "api"."get_switchview_descriptions"(inet,text) RETURNS TABLE(port text, description text) AS $$
+	use strict;
+	use warnings;
+	use Data::Dumper;
+	use Net::SNMP;
+	no warnings 'redefine';
+
+	# Connection information
+	#our $host = $ARGV[0];
+	#our $community = $ARGV[1];
+	our $host = $_[0];
+	our $community = $_[1];
+
+	# OID List
+	our $ifAliasList_OID = '1.3.6.1.2.1.31.1.1.1.18';
+	our $ifNameList_OID = '.1.3.6.1.2.1.31.1.1.1.1';
+
+	# Data
+	our %ifNameData;
+
+	# Establish session
+	our ($session,$error) = Net::SNMP->session (
+	     -hostname => $host,
+	     -community => $community,
+	);
+
+	# Get a list of all port names;
+	&get_names($session);
+	&get_aliases();
+
+	sub get_names() {
+		my $vlanSession = $_[0];
+
+		my $ifNameList = $vlanSession->get_table(-baseoid => $ifNameList_OID);
+		while (my($ifIndex,$ifName) = each(%$ifNameList)) {
+			$ifIndex =~ s/$ifNameList_OID\.//;
+			if($ifIndex =~ m/\d{5}/) {
+				$ifNameData{$ifIndex} = $ifName;
+			}
+		}
+	}
+
+	sub get_aliases() {
+		my $ifAliasList = $session->get_table(-baseoid => $ifAliasList_OID);
+		while (my($ifIndex,$ifAlias) = each(%$ifAliasList)) {
+			$ifIndex =~ s/$ifAliasList_OID\.//;
+			if($ifIndex =~ m/\d{5}/) {
+				#print "$ifNameData{$ifIndex} - $ifAlias\n";
+				my %row;
+				$row{port} = $ifNameData{$ifIndex};
+				$row{description} = $ifAlias;
+				return_next(\%row);
+			}
+		}
+	}
+
+	# Close initial session
+	$session->close();
+	return;
+$$ LANGUAGE 'plperlu';
+COMMENT ON FUNCTION "api"."get_switchview_descriptions"(inet,text) IS 'Get the descriptions of each port on a device';
