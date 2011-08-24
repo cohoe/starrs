edec80e6203e56b90f7f6a002f94f8dc6fc2a232
Grant Cohoe <cohoe@csh.rit.edu>
2011-08-24 07:40:29 -0400

    removed some old files

--- application/controllers/quick.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) 'No direct script access allowed';
-require_once(APPPATH . "libraries/core/ImpulseController.php");
-
-class Quick extends ImpulseController {
-	
-	
-}
-/* End of file quick.php */
-/* Location: ./application/controllers/quick.php */
