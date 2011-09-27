bf222b4e01c322fbca7fba9f5c5067fb1ef5186a
Grant Cohoe <cohoe@csh.rit.edu>
2011-09-27 11:11:29 -0400

    added model support for subnet utilization

--- application/models/API/Statistics/api_statistics_get.php
+++ application/models/API/Statistics/api_statistics_get.php
@@ -23,6 +23,13 @@ class Api_statistics_get extends ImpulseModel {
 		#return $query->result_array();
 		return $query->row_array();
 	}
+
+    public function subnet_utilization($subnet) {
+		$sql = "SELECT * FROM api.get_subnet_utilization() WHERE subnet = {$this->db->escape($subnet)}";
+		$query = $this->db->query($sql);
+		#return $query->result_array();
+		return $query->row_array();
+	}
 }
 /* End of file api_statistics_get.php */
 /* Location: ./application/models/API/Systems/api_statistics_get.php */
