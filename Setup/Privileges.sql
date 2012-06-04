2cc4668234d5e812e49d15b49a3b5029d520db46
Grant Cohoe <cohoe@csh.rit.edu>
2012-06-04 10:25:37 -0300

    added srv and cname tables to ACL

--- Setup/Privileges.sql
+++ Setup/Privileges.sql
@@ -71,6 +71,8 @@ GRANT SELECT,INSERT,UPDATE,DELETE ON "network"."switchview" TO "impulse_client";
 GRANT SELECT,INSERT,UPDATE,DELETE ON "network"."switchport_history" TO "impulse_client";
 GRANT SELECT,INSERT,UPDATE,DELETE ON "dhcp"."range_options" TO "impulse_client";
 GRANT SELECT,INSERT,UPDATE,DELETE ON "dns"."soa" TO "impulse_client";
+GRANT SELECT,INSERT,UPDATE,DELETE ON "dns"."srv" TO "impulse_client";
+GRANT SELECT,INSERT,UPDATE,DELETE ON "dns"."cname" TO "impulse_client";
 
 /* Special Data
 	Read or Write only, not both
