fec5ffc69bfd4c82df02e1c998b9a7830b5e83b4
Grant Cohoe <cohoe@csh.rit.edu>
2012-06-05 11:51:18 -0300

    Cant have a PK in zone_txt. Will need a unique

--- Setup/Tables.sql
+++ Setup/Tables.sql
@@ -368,8 +368,7 @@ CREATE TABLE "dns"."zone_txt"(
 "type" TEXT NOT NULL DEFAULT 'TXT',
 "ttl" INTEGER NOT NULL DEFAULT api.get_site_configuration('DNS_DEFAULT_TTL')::integer,
 "zone" TEXT NOT NULL DEFAULT api.get_site_configuration('DNS_DEFAULT_ZONE'),
-"address" INET DEFAULT '0.0.0.0',
-CONSTRAINT "zone_txt_pkey" PRIMARY KEY ("text","zone","hostname")
+"address" INET DEFAULT '0.0.0.0'
 )
 WITHOUT OIDS;
 
