2a1e911eadf7a9da95b83d61eb974313f6a420e2
Grant Cohoe <cohoe@csh.rit.edu>
2012-06-13 15:22:05 -0300

    Updated for #128

--- API/Management/api_management_modify.sql
+++ API/Management/api_management_modify.sql
@@ -6,7 +6,7 @@
 	1) Check privileges
 	2) Create directive
 */
-CREATE OR REPLACE FUNCTION "api"."modify_site_configuration"(input_directive text, input_value text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION "api"."modify_site_configuration"(input_directive text, input_value text) RETURNS SETOF "management"."configuration" AS $$
 	BEGIN
 		PERFORM api.create_log_entry('API','DEBUG','begin api.modify_site_configuration');
 
@@ -21,6 +21,7 @@ CREATE OR REPLACE FUNCTION "api"."modify_site_configuration"(input_directive tex
 
 		-- Done
 		PERFORM api.create_log_entry('API','DEBUG','finish api.modify_site_configuration');
+		RETURN QUERY (SELECT * FROM "management"."configuration" WHERE "option" = input_directive AND "value" = input_value);
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."modify_site_configuration"(text, text) IS 'Modify a site configuration directive';
