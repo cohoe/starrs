6facbef873b81e68764f2acab0b9f59212de9a5f
Grant Cohoe <cohoe@csh.rit.edu>
2012-06-14 17:33:15 -0300

    Added more functions

--- API/Network/api_network_get.sql
+++ API/Network/api_network_get.sql
@@ -190,7 +190,7 @@ CREATE OR REPLACE FUNCTION "api"."get_switchview_portindex"(inet, text, integer)
 $$ LANGUAGE 'plperlu';
 COMMENT ON FUNCTION "api"."get_switchview_portindex"(inet, text, integer) IS 'Get a mapping of port indexes to bridge indexes';
 
-CREATE OR REPLACE FUNCTION "api"."get_switchview_portnames"(inet, text, integer) RETURNS TABLE("ifindex" INTEGER, "ifname" TEXT) AS $$
+CREATE OR REPLACE FUNCTION "api"."get_switchview_port_names"(inet, text, integer) RETURNS TABLE("ifindex" INTEGER, "ifname" TEXT) AS $$
 	use strict;
 	use warnings;
 	use Net::SNMP;
@@ -232,7 +232,149 @@ CREATE OR REPLACE FUNCTION "api"."get_switchview_portnames"(inet, text, integer)
 	return undef;
 	
 $$ LANGUAGE 'plperlu';
-COMMENT ON FUNCTION "api"."get_switchview_portnames"(inet, text, integer) IS 'Map ifindexes to port names';
+COMMENT ON FUNCTION "api"."get_switchview_port_names"(inet, text, integer) IS 'Map ifindexes to port names';
+
+CREATE OR REPLACE FUNCTION "api"."get_switchview_port_descriptions"(inet, text) RETURNS TABLE("ifindex" INTEGER, "ifalias" TEXT) AS $$
+	use strict;
+	use warnings;
+	use Net::SNMP;
+	use Socket;
+
+	# Define OIDs
+	my $ifAlias = "1.3.6.1.2.1.31.1.1.1.18";
+
+	# Needed Variables
+	my $hostname = shift(@_) or die "Unable to get host";
+	my $community = shift(@_) or die "Unable to get READ community";
+
+	# Establish session
+	my ($session,$error) = Net::SNMP->session (
+		-hostname => "$hostname",
+		-community => "$community",
+	);
+
+	# Check that it did not error
+	if (!defined($session)) {
+		die $error;
+	}
+
+	# Get a list of all data
+	my $portAliases = $session->get_table(-baseoid => $ifAlias);
+
+	# Do something for each item of the list
+	while ( my ($portIndex, $portAlias) = each(%$portAliases)) {
+		$portIndex =~ s/$ifAlias\.//;
+		return_next({ifindex=>$portIndex, ifalias=>$portAlias});
+	}
+
+	# Gracefully disconnect
+	$session->close();
+	
+	# Return
+	return undef;
+	
+$$ LANGUAGE 'plperlu';
+COMMENT ON FUNCTION "api"."get_switchview_port_descriptions"(inet, text) IS 'Map ifindexes to port descriptions (or aliases in Cisco-land)';
+
+CREATE OR REPLACE FUNCTION "api"."get_switchview_port_operstatus"(inet, text) RETURNS TABLE("ifindex" INTEGER, "ifoperstatus" BOOLEAN) AS $$
+	use strict;
+	use warnings;
+	use Net::SNMP;
+	use Socket;
+
+	# Define OIDs
+	my $ifOperStatus = ".1.3.6.1.2.1.2.2.1.8";
+
+	# Needed Variables
+	my $hostname = shift(@_) or die "Unable to get host";
+	my $community = shift(@_) or die "Unable to get READ community";
+
+	# Establish session
+	my ($session,$error) = Net::SNMP->session (
+		-hostname => "$hostname",
+		-community => "$community",
+	);
+
+	# Check that it did not error
+	if (!defined($session)) {
+		die $error;
+	}
+
+	# Get a list of all data
+	my $portStates = $session->get_table(-baseoid => $ifOperStatus);
+
+	# Do something for each item of the list
+	while ( my ($portIndex, $portState) = each(%$portStates)) {
+		$portIndex =~ s/$ifOperStatus\.//;
+		if($portState-1 == 0) {
+			# Then its up
+			$portState = 1;
+		}
+		else {
+			# Then its down
+			$portState = 0;
+		}
+		return_next({ifindex=>$portIndex, ifoperstatus=>$portState});
+	}
+
+	# Gracefully disconnect
+	$session->close();
+	
+	# Return
+	return undef;
+	
+$$ LANGUAGE 'plperlu';
+COMMENT ON FUNCTION "api"."get_switchview_port_operstatus"(inet, text) IS 'Map ifindexes to port operational status';
+
+CREATE OR REPLACE FUNCTION "api"."get_switchview_port_adminstatus"(inet, text) RETURNS TABLE("ifindex" INTEGER, "ifadminstatus" BOOLEAN) AS $$
+	use strict;
+	use warnings;
+	use Net::SNMP;
+	use Socket;
+
+	# Define OIDs
+	my $ifAdminStatus = ".1.3.6.1.2.1.2.2.1.7";
+
+	# Needed Variables
+	my $hostname = shift(@_) or die "Unable to get host";
+	my $community = shift(@_) or die "Unable to get READ community";
+
+	# Establish session
+	my ($session,$error) = Net::SNMP->session (
+		-hostname => "$hostname",
+		-community => "$community",
+	);
+
+	# Check that it did not error
+	if (!defined($session)) {
+		die $error;
+	}
+
+	# Get a list of all data
+	my $portStates = $session->get_table(-baseoid => $ifAdminStatus);
+
+	# Do something for each item of the list
+	while ( my ($portIndex, $portState) = each(%$portStates)) {
+		$portIndex =~ s/$ifAdminStatus\.//;
+		if($portState-1 == 0) {
+			# Then its up
+			$portState = 1;
+		}
+		else {
+			# Then its down
+			$portState = 0;
+		}
+		return_next({ifindex=>$portIndex, ifadminstatus=>$portState});
+	}
+
+	# Gracefully disconnect
+	$session->close();
+	
+	# Return
+	return undef;
+	
+$$ LANGUAGE 'plperlu';
+COMMENT ON FUNCTION "api"."get_switchview_port_adminstatus"(inet, text) IS 'Map ifindexes to port administrative status';
 
 CREATE OR REPLACE FUNCTION "api"."get_switchview_cam"(input_host inet, input_community text) RETURNS SETOF "network"."cam" AS $$
 	DECLARE
@@ -261,238 +403,6 @@ COMMENT ON FUNCTION "api"."get_switchview_cam"(inet, text) IS 'Get all CAM data 
 ---- EVERYTHING ELSE IN THIS IS OLD CODE AND WILL PROBABLY BE REMOVED SOON ----
 -------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION "api"."get_network_switchport_view"(inet,text) RETURNS SETOF "network"."switchview_data" AS $$
-	use strict;
-	use warnings;
-	use Data::Dumper;
-	use Net::SNMP;
-	no warnings 'redefine';
-
-	# Connection information
-	#our $host = $ARGV[0];
-	#our $community = $ARGV[1];
-	our $host = $_[0];
-	our $community = $_[1];
-
-	# OID List
-	our $vlanList_OID = '.1.3.6.1.4.1.9.9.46.1.3.1.1.2';
-	our $macBridge_OID = '.1.3.6.1.2.1.17.4.3.1.1';
-	our $bridgeList_OID = '1.3.6.1.2.1.17.4.3.1.2';
-	our $ifIndexList_OID = '.1.3.6.1.2.1.17.1.4.1.2';
-	our $ifNameList_OID = '.1.3.6.1.2.1.31.1.1.1.1';
-	our $ifAdminStatus_OID = '.1.3.6.1.2.1.2.2.1.7';
-
-	our %ifNameData;
-	our %ifIndexData;
-	our %bridgeNumData;
-
-	# Establish session
-	my ($session,$error) = Net::SNMP->session (
-		 -hostname => $host,
-		 -community => $community,
-	);
-	
-	# Check that it did not error
-	if (!defined($session)) {
-		die $error;
-	}
-
-	# Get a list of all VLANs
-	my $vlanList = $session->get_table(-baseoid => $vlanList_OID);
-
-	while (my($vlanID,$opCode) = each(%$vlanList)) {
-		$vlanID =~ s/$vlanList_OID\.1\.//;
-		my ($vlanSession,$vlanError) = Net::SNMP->session (
-			-hostname => $host,
-			-community => "$community\@$vlanID",
-		);
-		&get_names($vlanSession);
-		&get_indexes($vlanSession);
-		&get_bridge_nums($vlanSession);
-		&get_macs($vlanSession);
-		$vlanSession->close();
-	}
-
-	sub get_macs() {
-		my $vlanSession = $_[0];
-
-		my $macBridgeList = $vlanSession->get_table(-baseoid => $macBridge_OID);
-		while (my($bridgeID,$mac) = each(%$macBridgeList)) {
-			$bridgeID =~ s/$macBridge_OID\.//;
-			$mac =~ s/^0x//;
-			if($mac =~ m/[0-9a-fA-F]{12}/) {
-				#print "$bridgeNumData{$bridgeID} - $mac\n";
-				$mac = &format_mac($mac);
-				my %row;
-				$row{port} = $bridgeNumData{$bridgeID};
-				$row{mac} = $mac;
-				return_next(\%row);
-			}
-		}
-	}
-
-	sub get_bridge_nums() {
-		my $vlanSession = $_[0];
-
-		my $bridgeList = $vlanSession->get_table(-baseoid => $bridgeList_OID);
-		while (my($bridgeID,$bridgeNum) = each(%$bridgeList)) {
-			$bridgeID =~ s/$bridgeList_OID\.//;
-			$bridgeNumData{$bridgeID} = $ifIndexData{$bridgeNum};
-		}
-	}
-
-	sub get_indexes() {
-		my $vlanSession = $_[0];
-
-		my $ifIndexList = $vlanSession->get_table(-baseoid => $ifIndexList_OID);
-		while (my($ifID,$ifIndex) = each(%$ifIndexList)) {
-			$ifID =~ s/$ifIndexList_OID\.//;
-			$ifIndexData{$ifID} = $ifNameData{$ifIndex};
-		}
-	}
-
-	sub get_names() {
-		my $vlanSession = $_[0];
-
-		my $ifNameList = $vlanSession->get_table(-baseoid => $ifNameList_OID);
-		while (my($ifIndex,$ifName) = each(%$ifNameList)) {
-			$ifIndex =~ s/$ifNameList_OID\.//;
-			if($ifIndex =~ m/\d{5}/) {
-				$ifNameData{$ifIndex} = $ifName;
-			}
-		}
-	}
-
-	sub format_mac() {
-			my $mac = $_[0];
-
-			$mac =~ s/(\w{2})(\w{2})(\w{2})(\w{2})(\w{2})(\w{2})/$1:$2:$3:$4:$5:$6/;
-
-			return $mac;
-		}
-
-	# Close initial session
-	$session->close();
-
-	return;
-$$ LANGUAGE 'plperlu';
-
-/* API - get_network_switchview_active_state */
-CREATE OR REPLACE FUNCTION "api"."get_network_switchview_active_state"(inet,text) RETURNS SETOF "network"."switchview_state_data" AS $$
-	use strict;
-	use warnings;
-	use Net::SNMP;
-	no warnings 'redefine';
-
-	# Connection information
-	our $host = $_[0];
-	our $community = $_[1];
-
-	# OID List
-	our $ifName_OID = '.1.3.6.1.2.1.31.1.1.1.1';
-	our $ifOperStatus_OID = '.1.3.6.1.2.1.2.2.1.8';
-
-	# Arrays of data
-	my %ifIndexData;
-	my %ifNameData;
-
-	# Establish session
-	my ($session,$error) = Net::SNMP->session (
-	     -hostname => $host,
-	     -community => $community,
-	);
-
-	# Get a listing of all port indexes and their current state
-	my $portIndexStatus = $session->get_table(-baseoid => $ifOperStatus_OID);
-	while (my($ifIndex,$portState) = each(%$portIndexStatus)) {
-		$ifIndex =~ s/$ifOperStatus_OID\.//;
-		if($ifIndex =~ m/\d{5}/) {
-			$ifIndexData{$ifIndex} = $portState;
-		}
-	}
-
-	# Get a list of all port names?
-	my $portNames = $session->get_table(-baseoid => $ifName_OID);
-	while (my($ifIndex,$ifName) = each(%$portNames)) {
-		$ifIndex =~ s/$ifName_OID\.//;
-		if($ifIndex =~ m/\d{5}/) {
-			$ifNameData{$ifIndex} = $ifName;
-		}
-	}
-
-	# Map the data;
-	while (my ($ifIndex,$ifName) = each(%ifNameData)) {
-		my %row;
-		$row{port} = $ifName;
-		$row{state}=0;
-		if($ifIndexData{$ifIndex}-1==0) {
-			$row{state}=1;
-		}
-		return_next(\%row);
-	}
-
-	# Close session
-	$session->close();
-	return;
-$$ LANGUAGE 'plperlu';
-COMMENT ON FUNCTION "api"."get_network_switchview_active_state"(inet,text) IS 'Get the current state of the switchports on a system';
-
-/* API - get_network_switchview_admin_state */
-CREATE OR REPLACE FUNCTION "api"."get_network_switchview_admin_state"(inet,text) RETURNS SETOF "network"."switchview_state_data" AS $$
-	use strict;
-	use warnings;
-	use Net::SNMP;
-	no warnings 'redefine';
-
-	# Connection information
-	our $host = $_[0];
-	our $community = $_[1];
-
-	# OID List
-	our $ifName_OID = '.1.3.6.1.2.1.31.1.1.1.1';
-	our $ifAdminStatus_OID = '.1.3.6.1.2.1.2.2.1.7';
-
-	# Arrays of data
-	my %ifIndexData;
-	my %ifNameData;
-
-	# Establish session
-	my ($session,$error) = Net::SNMP->session (
-	     -hostname => $host,
-	     -community => $community,
-	);
-
-	# Get a listing of all port indexes and their current state
-	my $portIndexStatus = $session->get_table(-baseoid => $ifAdminStatus_OID);
-	while (my($ifIndex,$portState) = each(%$portIndexStatus)) {
-		$ifIndex =~ s/$ifAdminStatus_OID\.//;
-		if($ifIndex =~ m/\d{5}/) {
-			$ifIndexData{$ifIndex} = $portState;
-		}
-	}
-
-	# Get a list of all port names?
-	my $portNames = $session->get_table(-baseoid => $ifName_OID);
-	while (my($ifIndex,$ifName) = each(%$portNames)) {
-		$ifIndex =~ s/$ifName_OID\.//;
-		if($ifIndex =~ m/\d{5}/) {
-			$ifNameData{$ifIndex} = $ifName;
-		}
-	}
-
-	# Map the data
-	while (my ($ifIndex,$ifName) = each(%ifNameData)) {
-		my %row;
-		$row{port} = $ifName;
-		$row{state} = $ifIndexData{$ifIndex};
-		return_next(\%row);
-	}
-
-	# Close session
-	$session->close();
-	return;
-$$ LANGUAGE 'plperlu';
-COMMENT ON FUNCTION "api"."get_network_switchview_admin_state"(inet,text) IS 'Get the current administrative state of the switchports on a system';
 
 /* API - get_network_switchport_types */
 CREATE OR REPLACE FUNCTION "api"."get_network_switchport_types"() RETURNS SETOF TEXT AS $$
@@ -563,68 +473,6 @@ CREATE OR REPLACE FUNCTION "api"."get_network_switchport"(input_system_name text
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_network_switchport"(text,text) IS 'Get a switchport';
 
-CREATE OR REPLACE FUNCTION "api"."get_switchview_descriptions"(inet,text) RETURNS TABLE(port text, description text) AS $$
-	use strict;
-	use warnings;
-	use Data::Dumper;
-	use Net::SNMP;
-	no warnings 'redefine';
-
-	# Connection information
-	#our $host = $ARGV[0];
-	#our $community = $ARGV[1];
-	our $host = $_[0];
-	our $community = $_[1];
-
-	# OID List
-	our $ifAliasList_OID = '1.3.6.1.2.1.31.1.1.1.18';
-	our $ifNameList_OID = '.1.3.6.1.2.1.31.1.1.1.1';
-
-	# Data
-	our %ifNameData;
-
-	# Establish session
-	our ($session,$error) = Net::SNMP->session (
-	     -hostname => $host,
-	     -community => $community,
-	);
-
-	# Get a list of all port names;
-	&get_names($session);
-	&get_aliases();
-
-	sub get_names() {
-		my $vlanSession = $_[0];
-
-		my $ifNameList = $vlanSession->get_table(-baseoid => $ifNameList_OID);
-		while (my($ifIndex,$ifName) = each(%$ifNameList)) {
-			$ifIndex =~ s/$ifNameList_OID\.//;
-			if($ifIndex =~ m/\d{5}/) {
-				$ifNameData{$ifIndex} = $ifName;
-			}
-		}
-	}
-
-	sub get_aliases() {
-		my $ifAliasList = $session->get_table(-baseoid => $ifAliasList_OID);
-		while (my($ifIndex,$ifAlias) = each(%$ifAliasList)) {
-			$ifIndex =~ s/$ifAliasList_OID\.//;
-			if($ifIndex =~ m/\d{5}/) {
-				#print "$ifNameData{$ifIndex} - $ifAlias\n";
-				my %row;
-				$row{port} = $ifNameData{$ifIndex};
-				$row{description} = $ifAlias;
-				return_next(\%row);
-			}
-		}
-	}
-
-	# Close initial session
-	$session->close();
-	return;
-$$ LANGUAGE 'plperlu';
-COMMENT ON FUNCTION "api"."get_switchview_descriptions"(inet,text) IS 'Get the descriptions of each port on a device';
-
 CREATE OR REPLACE FUNCTION "api"."get_snmp_rw"(input_system_name text) RETURNS TEXT AS $$
 	BEGIN
 		RETURN (SELECT "snmp_rw_community" FROM "network"."switchview" WHERE "system_name" = input_system_name);
@@ -648,3 +496,4 @@ CREATE OR REPLACE FUNCTION "api"."get_network_switchport_history"(input_system_n
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_network_switchport_history"(text,text) IS 'Get the MAC address history of a port';
+
