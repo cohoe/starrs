66c492f22e3a64c3134f27396525291981515441
Grant Cohoe <cohoe@csh.rit.edu>
2012-06-14 13:21:24 -0300

    Removed a bunch of useless crap

--- Setup/Types.sql
+++ Setup/Types.sql
@@ -52,45 +52,9 @@ COMMENT ON TYPE "dhcp"."dhcpd_zones" IS 'Zone information for dhcpd';
 CREATE TYPE "dhcp"."dhcpd_class_options" AS (option text, value text);
 COMMENT ON TYPE "dhcp"."dhcpd_class_options" IS 'Get class options for the dhcpd.conf';
 
-/* DNS - mx_data */
-CREATE TYPE "dns"."mx_data" AS (hostname varchar(63), zone text, address inet, type text, preference integer, ttl integer, owner text, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "dns"."mx_data" IS 'All MX data';
-
-/* DNS - ns_data */
-CREATE TYPE "dns"."ns_data" AS (hostname varchar(63), zone text, address inet, type text, isprimary boolean, ttl integer, owner text, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "dns"."ns_data" IS 'All NS data';
-
-/* DNS - txt_data */
-CREATE TYPE "dns"."txt_data" AS (hostname varchar(63), zone text, address inet, type text, text text, ttl integer, owner text, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "dns"."txt_data" IS 'All text (TXT) data';
-
-/* DNS - cname_data */
-CREATE TYPE "dns"."cname_data" AS (alias varchar(63), hostname varchar(63), zone text, address inet, ttl integer, owner text, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "dns"."cname_data" IS 'All cname record data';
-
-/* DNS - srv_data */
-CREATE TYPE "dns"."srv_data" AS (alias varchar(63), hostname varchar(63), zone text, priority integer, weight integer, port integer, address inet, ttl integer, owner text, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "dns"."srv_data" IS 'All srv record data';
-
-/* DNS - a_data */
-CREATE TYPE "dns"."a_data" AS (hostname varchar(63), zone text, address inet, type text, ttl integer, owner text, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "dns"."a_data" IS 'All address (A/AAAA) data';
-
 /* DNS - zone_data */
 CREATE TYPE "dns"."zone_audit_data" AS (host TEXT, ttl INTEGER, type TEXT, address INET, port INTEGER, weight INTEGER, priority INTEGER, preference INTEGER, target TEXT, text TEXT, contact TEXT, serial TEXT, refresh INTEGER, retry INTEGER, expire INTEGER, minimum INTEGER);
 COMMENT ON TYPE "dns"."zone_audit_data" IS 'All DNS zone data for auditing purposes';
-
-/* Systems - interface_address_data */
-CREATE TYPE "systems"."interface_address_data" AS (mac macaddr, address inet, family integer, config text, class text, isprimary boolean, comment text, renew_date date, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "systems"."interface_address_data" IS 'Interface address data';
-
-/* Systems - interface_data */
-CREATE TYPE "systems"."interface_data" AS (system_name text, mac macaddr, name text, comment text, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "systems"."interface_data" IS 'System interface information';
-
-/* Systems - system_data */
-CREATE TYPE "systems"."system_data" AS (system_name text, type text, family text, os_name text, owner text, comment text, renew_date date, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "systems"."system_data" IS 'System information';
 
 /* Systems - os_family_distribution */
 CREATE TYPE "systems"."os_family_distribution" AS (family text, count integer, percentage integer);
@@ -100,20 +64,7 @@ COMMENT ON TYPE "systems"."os_family_distribution" IS 'OS distribution statistic
 CREATE TYPE "systems"."os_distribution" AS (name text, count integer, percentage integer);
 COMMENT ON TYPE "systems"."os_distribution" IS 'OS distribution statistics';
 
-CREATE TYPE "dhcp"."config_type_data" AS (config text, family integer, comment text, date_created timestamp, date_modified timestamp, last_modifier text);
-
-CREATE TYPE "dhcp"."class_data" AS (class text, comment text, date_created timestamp, date_modified timestamp, last_modifier text);
-
-CREATE TYPE "ip"."range_data" AS (name text, first_ip inet, last_ip inet, subnet cidr, use varchar(4), class text, comment text, date_created timestamp, date_modified timestamp, last_modifier text);
-
-CREATE TYPE "ip"."subnet_data" AS (name text, subnet cidr, zone text, owner text, autogen boolean, dhcp_enable boolean, comment text, date_created timestamp, date_modified timestamp, last_modifier text);
-COMMENT ON TYPE "ip"."subnet_data" IS 'IP subnet data';
-
-CREATE TYPE "dns"."zone_data" AS (zone text, keyname text, forward boolean, shared boolean, owner text, comment text, date_created timestamp, date_modified timestamp, last_modifier text);
-
-CREATE TYPE "dns"."key_data" AS (keyname text, key text, comment text, owner text, date_created timestamp, date_modified timestamp, last_modifier text);
-
-CREATE TYPE "dhcp"."option_data" AS (option text, value text, date_created timestamp, date_modified timestamp, last_modifier text);
+CREATE TYPE "api"."search_data" AS (system_name text, mac macaddr, address inet, system_owner text, system_last_modifier text, range text, hostname varchar(63), zone text, dns_owner text, dns_last_modifier text);
 
 CREATE TYPE "network"."switchview_data" AS (port text, mac macaddr);
 
@@ -122,5 +73,3 @@ CREATE TYPE "network"."switchport_data" AS (system_name text, port_name text, ty
 CREATE TYPE "network"."switchview_setting_data" AS (snmp_ro_community text, snmp_rw_community text, enable boolean);
 
 CREATE TYPE "network"."switchview_state_data" AS (port text, state boolean);
-
-CREATE TYPE "api"."search_data" AS (system_name text, mac macaddr, address inet, system_owner text, system_last_modifier text, range text, hostname varchar(63), zone text, dns_owner text, dns_last_modifier text);
