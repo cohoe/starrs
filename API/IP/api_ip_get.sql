80095e997bfa02fa2b764e8bd471f26b5dce2d63
Grant Cohoe <cohoe@csh.rit.edu>
2012-06-19 10:15:48 -0300

    Moved statistics functions for #144

--- API/IP/api_ip_get.sql
+++ API/IP/api_ip_get.sql
@@ -174,3 +174,31 @@ CREATE OR REPLACE FUNCTION "api"."get_ip_range_total"(input_range text) RETURNS 
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_ip_range_total"(text) IS 'Get the number of possible addresses in a particiular range';
+
+CREATE OR REPLACE FUNCTION api.get_range_utilization() RETURNS TABLE("name" text, inuse integer, free integer, total integer) AS $$
+	BEGIN
+		RETURN QUERY (SELECT  api.get_address_range("systems"."interface_addresses"."address") AS "name",
+			COUNT(api.get_address_range("systems"."interface_addresses"."address")) AS "inuse",
+			(api.get_ip_range_total(api.get_address_range("systems"."interface_addresses"."address")) - COUNT(api.get_address_range("systems"."interface_addresses"."address"))) AS "free",
+			api.get_ip_range_total(api.get_address_range("systems"."interface_addresses"."address")) AS "total"
+			FROM "systems"."interface_addresses" 
+			JOIN "ip"."ranges" 
+			ON "ip"."ranges"."name" = api.get_address_range("address") 
+			WHERE api.get_address_range("address") 
+			IN (SELECT "ip"."ranges"."name" FROM "ip"."ranges")
+			GROUP BY api.get_address_range("systems"."interface_addresses"."address")
+			ORDER BY "name");
+	END;
+$$ LANGUAGE plpgsql;
+COMMENT ON FUNCTION api.get_range_utilization() IS 'Get statistics on range utilization';
+
+CREATE OR REPLACE FUNCTION api.get_subnet_utilization() RETURNS TABLE("subnet" cidr, inuse integer, free integer, total integer) AS $$
+	BEGIN
+		RETURN QUERY (SELECT "ip"."subnets"."subnet",
+			(SELECT COUNT("systems"."interface_addresses"."address") FROM "systems"."interface_addresses" WHERE "systems"."interface_addresses"."address" << "ip"."subnets"."subnet")::integer as "inuse",
+			((SELECT count("ip"."addresses"."address") FROM "ip"."addresses" WHERE "ip"."addresses"."address" << "ip"."subnets"."subnet")::integer - (SELECT count("systems"."interface_addresses"."address") FROM "systems"."interface_addresses" WHERE "systems"."interface_addresses"."address" << "ip"."subnets"."subnet")::integer) AS "free",
+			(SELECT COUNT("ip"."addresses"."address") FROM "ip"."addresses" WHERE "ip"."addresses"."address" << "ip"."subnets"."subnet")::integer AS "total"
+			FROM "ip"."subnets" GROUP BY "ip"."subnets"."subnet" ORDER BY "ip"."subnets"."subnet");
+	END;
+$$ LANGUAGE plpgsql;
+COMMENT ON FUNCTION api.get_subnet_utilization() IS 'Get statistics on subnet utilization';
