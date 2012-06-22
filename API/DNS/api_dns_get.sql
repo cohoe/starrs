d77a4b3269a7f1f4ecab7a933cf79e77f36cce55
Grant Cohoe <cohoe@csh.rit.edu>
2012-06-22 09:40:13 -0300

    Updated for #148

--- API/DNS/api_dns_get.sql
+++ API/DNS/api_dns_get.sql
@@ -1,7 +1,11 @@
 /* API - get_dns_mx */
 CREATE OR REPLACE FUNCTION "api"."get_dns_mx"(input_address inet) RETURNS SETOF "dns"."mx" AS $$
 	BEGIN
-		RETURN QUERY (SELECT * FROM "dns"."mx" WHERE "address" = input_address ORDER BY "preference");
+		IF input_address IS NULL THEN
+			RETURN QUERY (SELECT * FROM "dns"."mx" ORDER BY "preference");
+		ELSE
+			RETURN QUERY (SELECT * FROM "dns"."mx" WHERE "address" = input_address ORDER BY "preference");
+		END IF;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_mx"(inet) IS 'Get all data pertanent to DNS MX records for an address';
@@ -9,7 +13,11 @@ COMMENT ON FUNCTION "api"."get_dns_mx"(inet) IS 'Get all data pertanent to DNS M
 /* API - get_dns_zone_ns */
 CREATE OR REPLACE FUNCTION "api"."get_dns_zone_ns"(input_zone text) RETURNS SETOF "dns"."ns" AS $$
 	BEGIN
-		RETURN QUERY (SELECT * FROM "dns"."ns" WHERE "zone" = input_zone ORDER BY "nameserver");
+		IF input_address IS NULL THEN
+			RETURN QUERY (SELECT * FROM "dns"."ns" ORDER BY "nameserver");
+		ELSE
+			RETURN QUERY (SELECT * FROM "dns"."ns" WHERE "zone" = input_zone ORDER BY "nameserver");
+		END IF;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_zone_ns"(text) IS 'Get all DNS NS records for a zone';
@@ -17,7 +25,11 @@ COMMENT ON FUNCTION "api"."get_dns_zone_ns"(text) IS 'Get all DNS NS records for
 /* API - get_dns_text */
 CREATE OR REPLACE FUNCTION "api"."get_dns_txt"(input_address inet) RETURNS SETOF "dns"."txt" AS $$
 	BEGIN
-		RETURN QUERY (SELECT * FROM "dns"."txt" WHERE "address" = input_address ORDER BY "text");
+		IF input_address IS NULL THEN
+			RETURN QUERY (SELECT * FROM "dns"."txt" ORDER BY "text");
+		ELSE
+			RETURN QUERY (SELECT * FROM "dns"."txt" WHERE "address" = input_address ORDER BY "text");
+		END IF;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_txt"(inet) IS 'Get all DNS TXT records for an address';
@@ -25,7 +37,11 @@ COMMENT ON FUNCTION "api"."get_dns_txt"(inet) IS 'Get all DNS TXT records for an
 /* API - get_dns_srv */
 CREATE OR REPLACE FUNCTION "api"."get_dns_srv"(input_address inet) RETURNS SETOF "dns"."srv" AS $$
 	BEGIN
-		RETURN QUERY (SELECT * FROM "dns"."srv" WHERE "address" = input_address ORDER BY "alias" ASC);
+		IF input_address IS NULL THEN
+			RETURN QUERY (SELECT * FROM "dns"."srv" ORDER BY "alias");
+		ELSE
+			RETURN QUERY (SELECT * FROM "dns"."srv" WHERE "address" = input_address ORDER BY "alias" ASC);
+		END IF;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_srv"(inet) IS 'Get all DNS SRV records for an address';
@@ -33,7 +49,11 @@ COMMENT ON FUNCTION "api"."get_dns_srv"(inet) IS 'Get all DNS SRV records for an
 /* API - get_dns_cname */
 CREATE OR REPLACE FUNCTION "api"."get_dns_cname"(input_address inet) RETURNS SETOF "dns"."cname" AS $$
 	BEGIN
-		RETURN QUERY (SELECT * FROM "dns"."cname" WHERE "address" = input_address ORDER BY "alias" ASC);
+		IF input_address IS NULL THEN
+			RETURN QUERY (SELECT * FROM "dns"."cname" ORDER BY "alias");
+		ELSE
+			RETURN QUERY (SELECT * FROM "dns"."cname" WHERE "address" = input_address ORDER BY "alias" ASC);
+		END IF;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_cname"(inet) IS 'Get all DNS CNAME records for an address';
@@ -122,7 +142,11 @@ COMMENT ON FUNCTION "api"."get_dns_key"(text) IS 'Get DNS key data';
 
 CREATE OR REPLACE FUNCTION "api"."get_dns_soa"(input_zone text) RETURNS SETOF "dns"."soa" AS $$
 	BEGIN
-		RETURN QUERY (SELECT * FROM "dns"."soa" WHERE "dns"."soa"."zone" = input_zone);
+		IF input_address IS NULL THEN
+			RETURN QUERY (SELECT * FROM "dns"."soa" ORDER BY "zone");
+		ELSE
+			RETURN QUERY (SELECT * FROM "dns"."soa" WHERE "dns"."soa"."zone" = input_zone);
+		END IF;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_soa"(text) IS 'Get the SOA record of a DNS zone';
@@ -130,14 +154,22 @@ COMMENT ON FUNCTION "api"."get_dns_soa"(text) IS 'Get the SOA record of a DNS zo
 /* API - get_dns_text */
 CREATE OR REPLACE FUNCTION "api"."get_dns_zone_txt"(input_zone text) RETURNS SETOF "dns"."zone_txt" AS $$
 	BEGIN
-		RETURN QUERY (SELECT * FROM "dns"."zone_txt" WHERE "zone" = input_zone ORDER BY "hostname" ASC);
+		IF input_address IS NULL THEN
+			RETURN QUERY (SELECT * FROM "dns"."zone_txt" ORDER BY "hostname");
+		ELSE
+			RETURN QUERY (SELECT * FROM "dns"."zone_txt" WHERE "zone" = input_zone ORDER BY "hostname" ASC);
+		END IF;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_zone_txt"(text) IS 'Get all DNS TXT records specifically for a zone';
 
 CREATE OR REPLACE FUNCTION "api"."get_dns_zone_a"(input_zone text) RETURNS SETOF "dns"."zone_a" AS $$
 	BEGIN
-		RETURN QUERY (SELECT * FROM "dns"."zone_a" WHERE "zone" = input_zone ORDER BY "address");
+		IF input_address IS NULL THEN
+			RETURN QUERY (SELECT * FROM "dns"."zone_a" ORDER BY "address");
+		ELSE
+			RETURN QUERY (SELECT * FROM "dns"."zone_a" WHERE "zone" = input_zone ORDER BY "address");
+		END IF;
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_dns_zone_a"(text) IS 'Get all DNS address records for a zone';
