f3a14cf89c6d63f4495e3eb131c74d426c03386a
Grant Cohoe <cohoe@csh.rit.edu>
2012-07-15 15:11:56 -0400

    new utilization functions

--- API/IP/api_ip_get.sql
+++ API/IP/api_ip_get.sql
@@ -175,30 +175,33 @@ CREATE OR REPLACE FUNCTION "api"."get_ip_range_total"(input_range text) RETURNS 
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."get_ip_range_total"(text) IS 'Get the number of possible addresses in a particiular range';
 
-CREATE OR REPLACE FUNCTION api.get_range_utilization() RETURNS TABLE("name" text, inuse integer, free integer, total integer) AS $$
+CREATE OR REPLACE FUNCTION api.get_range_utilization(input_range text) RETURNS TABLE(inuse integer, free integer, total integer) AS $$
 	BEGIN
-		RETURN QUERY (SELECT  api.get_address_range("systems"."interface_addresses"."address") AS "name",
-			COUNT(api.get_address_range("systems"."interface_addresses"."address")) AS "inuse",
-			(api.get_ip_range_total(api.get_address_range("systems"."interface_addresses"."address")) - COUNT(api.get_address_range("systems"."interface_addresses"."address"))) AS "free",
-			api.get_ip_range_total(api.get_address_range("systems"."interface_addresses"."address")) AS "total"
+		RETURN QUERY (
+			SELECT COUNT("systems"."interface_addresses"."address")::integer AS "inuse",
+			(api.get_ip_range_total(input_range) - COUNT("systems"."interface_addresses"."address"))::integer AS "free",
+			api.get_ip_range_total(input_range)::integer AS "total"
 			FROM "systems"."interface_addresses" 
-			JOIN "ip"."ranges" 
-			ON "ip"."ranges"."name" = api.get_address_range("address") 
-			WHERE api.get_address_range("address") 
-			IN (SELECT "ip"."ranges"."name" FROM "ip"."ranges")
-			GROUP BY api.get_address_range("systems"."interface_addresses"."address")
-			ORDER BY "name");
+			WHERE api.get_address_range("address") = input_range);
 	END;
 $$ LANGUAGE plpgsql;
-COMMENT ON FUNCTION api.get_range_utilization() IS 'Get statistics on range utilization';
+COMMENT ON FUNCTION api.get_range_utilization(text) IS 'Get statistics on range utilization';
 
-CREATE OR REPLACE FUNCTION api.get_subnet_utilization() RETURNS TABLE("subnet" cidr, inuse integer, free integer, total integer) AS $$
+CREATE OR REPLACE FUNCTION api.get_subnet_utilization(input_subnet cidr) RETURNS TABLE(inuse integer, free integer, total integer) AS $$
+	DECLARE
+		addrcount INTEGER;
 	BEGIN
-		RETURN QUERY (SELECT "ip"."subnets"."subnet",
-			(SELECT COUNT("systems"."interface_addresses"."address") FROM "systems"."interface_addresses" WHERE "systems"."interface_addresses"."address" << "ip"."subnets"."subnet")::integer as "inuse",
-			((SELECT count("ip"."addresses"."address") FROM "ip"."addresses" WHERE "ip"."addresses"."address" << "ip"."subnets"."subnet")::integer - (SELECT count("systems"."interface_addresses"."address") FROM "systems"."interface_addresses" WHERE "systems"."interface_addresses"."address" << "ip"."subnets"."subnet")::integer) AS "free",
-			(SELECT COUNT("ip"."addresses"."address") FROM "ip"."addresses" WHERE "ip"."addresses"."address" << "ip"."subnets"."subnet")::integer AS "total"
-			FROM "ip"."subnets" GROUP BY "ip"."subnets"."subnet" ORDER BY "ip"."subnets"."subnet");
+		-- Total
+		SELECT COUNT("address")::integer INTO addrcount
+		FROM "ip"."addresses" WHERE "address" << input_subnet;
+		
+		RETURN QUERY (
+			SELECT COUNT("systems"."interface_addresses"."address"):: integer AS "inuse",
+			addrcount - COUNT("systems"."interface_addresses"."address"):: integer as "free",
+			addrcount AS "total"
+			FROM "systems"."interface_addresses"
+			WHERE "address" << input_subnet
+		);
 	END;
 $$ LANGUAGE plpgsql;
-COMMENT ON FUNCTION api.get_subnet_utilization() IS 'Get statistics on subnet utilization';
+COMMENT ON FUNCTION api.get_subnet_utilization(cidr) IS 'Get statistics on subnet utilization';
