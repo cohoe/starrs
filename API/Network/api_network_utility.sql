d80fa0caa38fc1b956a37b884dd4f9d0aba9b4b5
Grant Cohoe <cohoe@csh.rit.edu>
2012-07-25 14:23:41 -0400

    bux fixes

--- API/Network/api_network_utility.sql
+++ API/Network/api_network_utility.sql
@@ -6,11 +6,11 @@ CREATE OR REPLACE FUNCTION "api"."reload_cam"(input_system_name text) RETURNS VO
 			END IF;
 		END IF;
 
-		INSERT INTO "network"."cam_cache" ("system_name","mac","vlan","timestamp","ifindex") (
-			SELECT input_system_name,mac,vlan,localtimestamp(0),ifindex
+		INSERT INTO "network"."cam_cache" ("system_name","mac","vlan","date_created","date_modified","last_modifier","ifindex") (
+			SELECT input_system_name,mac,vlan,localtimestamp(0),localtimestamp(0),api.get_current_user(),ifindex
 			FROM api.get_switchview_device_cam(input_system_name)
 		);
-		DELETE FROM "network"."cam_cache" WHERE "system_name" = input_system_name AND "timestamp" != localtimestamp(0);
+		DELETE FROM "network"."cam_cache" WHERE "system_name" = input_system_name AND "date_created" != localtimestamp(0);
 	END;
 $$ LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION "api"."reload_cam"(text) IS 'Reload the cam cache for a system';
